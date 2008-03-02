6bbacd47b8eab9a8fac98d63de1ef53b1a7102f7
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-03-02 23:27:59 +0000

    Gridworld seems to work. However policy evaluation does not seem to calculate state-action values properly.

--- trunk/src/algorithms/tests/policy_evaluation.cc
+++ trunk/src/algorithms/tests/policy_evaluation.cc
@@ -12,6 +12,7 @@
 
 #ifdef MAKE_MAIN
 #include "PolicyEvaluation.h"
+#include "ValueIteration.h"
 #include "Gridworld.h"
 #include "InventoryManagement.h"
 #include "DiscretePolicy.h"
@@ -20,11 +21,17 @@ int main (void)
 {
     int period = 30;
     int max_items = 10;
-    real gamma = 0.9;
+    real gamma = 1.0;
     real demand = 0.1;
+    real random = 0.0;
+    real pit = -100.0;
+    uint width=8;
+    uint height=8;
     InventoryManagement inventory_management (period, max_items, demand);
-    
-    const DiscreteMDP* mdp = inventory_management.getMDP();
+
+    Gridworld grid_world("maze1", width, height, 4, random, pit);
+    //const DiscreteMDP* mdp = inventory_management.getMDP();
+    const DiscreteMDP* mdp = grid_world.getMDP();
     
     int n_states = mdp->GetNStates();
     int n_actions = mdp->GetNActions();
@@ -40,9 +47,11 @@ int main (void)
     DiscretePolicy* policy = new FixedDiscretePolicy(p);
 
     PolicyEvaluation policy_evaluation(policy, mdp, gamma);
+    //ValueIteration policy_evaluation(mdp, gamma);
 
-    policy_evaluation.ComputeStateValues(0.01, 1000);
-    std::vector<float> Q(n_actions);
+    policy_evaluation.ComputeStateValues(0.001, 1000);
+    std::vector<real> Q(n_actions);
+#if 0
     for (int s=0; s<n_states; s++) {
         for (int a=0; a<n_actions; a++) {
             Q[a] = policy_evaluation.getValue(s,a);
@@ -53,6 +62,21 @@ int main (void)
                   << a_max << ":" << Q[a_max]
                   << std::endl;
     }
+#else
+    for (int y=0; y<height; y++) {
+        for (int x=0; x<width; x++) {
+            int s= x + y*width;
+            for (int a=0; a<n_actions; a++) {
+                Q[a] = policy_evaluation.getValue(s,a);
+            }
+            int a_max = ArgMax(Q);
+            real Q_max = Q[a_max];
+            real V = policy_evaluation.getValue(s);
+            printf ("%+.1f ", V);
+        }
+        printf ("\n");
+    }
+#endif
     
 }
 
