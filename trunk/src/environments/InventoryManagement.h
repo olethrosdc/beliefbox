461c153ffd2c790d0e01ca89a816761a012a8dc2
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-03-02 20:03:01 +0000

    Added inventory management and binomial distribution.

---
+++ trunk/src/environments/InventoryManagement.h
@@ -0,0 +1,35 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2007 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef INVENTORY_MANAGEMENT_H
+#define INVENTORY_MANAGEMENT_H
+
+#include "DiscreteMDP.h"
+#include "BinomialDistribution.h"
+#include <string>
+#include <vector>
+
+// The inventory management task.
+class InventoryManagement {
+protected:
+    int period;
+    int max_items;
+    real demand;
+    std::vector<BinomialDistribution*> dist;
+    DiscreteMDP* mdp;
+public:
+    InventoryManagement(int period_, int max_items_, real demand_);
+    ~InventoryManagement();
+    DiscreteMDP* GetMDP() {return mdp;}
+};
+
+#endif
---
+++ trunk/src/statistics/BinomialDistribution.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C++; -*- */
+/* VER: $Id: Distribution.h,v 1.3 2006/11/06 15:48:53 cdimitrakakis Exp cdimitrakakis $*/
+// copyright (c) 2004-2006 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef BINOMIAL_DISTRIBUTION_H
+#define BINOMIAL_DISTRIBUTION_H
+
+#include "Distribution.h"
+
+class BinomialDistribution : public ParametricDistribution
+{
+public:
+    real p;
+    long t;
+    real s;
+    BinomialDistribution(){p=0.5f;t=1;}
+    BinomialDistribution(real p, long t, real s=1.0);
+    virtual ~BinomialDistribution() {}
+    virtual void setMean(real mean) {p = mean;}
+    virtual void setVariance(real var) {} ///< set variance in a magic way
+    virtual real generate();
+    virtual real pdf(real x);
+    virtual real getMean() {return s*t*p;}
+    void setPeriod(long t);
+};
+
+#endif
