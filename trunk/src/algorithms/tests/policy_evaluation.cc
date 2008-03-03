a54a2a31e9c5444350a5158b84939c32e822b398
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-03-03 13:25:45 +0000

    Added mazes. Did TeX output for policy evaluation test.

--- trunk/src/algorithms/tests/policy_evaluation.cc
+++ trunk/src/algorithms/tests/policy_evaluation.cc
@@ -25,11 +25,13 @@ int main (void)
     real demand = 0.1;
     real random = 0.0;
     real pit = -100.0;
-    uint width=4;
-    uint height=4;
+    real goal = 0.0;
+    real step = -0.1;
+    uint width = 8;
+    uint height = 8;
     InventoryManagement inventory_management (period, max_items, demand);
 
-    Gridworld grid_world("maze0", width, height, 4, random, pit);
+    Gridworld grid_world("maze1", width, height, 4, random, pit, goal, step);
     //const DiscreteMDP* mdp = inventory_management.getMDP();
     const DiscreteMDP* mdp = grid_world.getMDP();
     
@@ -41,18 +43,18 @@ int main (void)
         p[s].Resize(n_actions);
         for (int a=0; a<n_actions; a++) {
             p[s][a] = 1.0 / (real) n_actions;
-	    printf ("R(%d,%d) = %f\n", s, a, mdp->getExpectedReward(s, a));
+	    //printf ("R(%d,%d) = %f\n", s, a, mdp->getExpectedReward(s, a));
         }
     }
 
     DiscretePolicy* policy = new FixedDiscretePolicy(p);
 
-#if 1
+#if 0
     PolicyEvaluation policy_evaluation(policy, mdp, gamma);
 
 #else
     ValueIteration policy_evaluation(mdp, gamma);
-    policy_evaluation.ComputeStateActionValues(0.001, 1000);
+    //policy_evaluation.ComputeStateActionValues(0.001, 1000);
 #endif
 
     std::vector<real> Q(n_actions);
@@ -68,40 +70,56 @@ int main (void)
                   << std::endl;
     }
 #else
-    for (int iter=0; iter < 10; iter++) {
+    for (int iter=0; iter < 100; iter++) {
 	printf ("ITER: %d\n", iter);
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {
+    for (uint y=0; y<height; y++) {
+        for (uint x=0; x<width; x++) {
             int s= x + y*width;
             for (int a=0; a<n_actions; a++) {
                 Q[a] = policy_evaluation.getValue(s,a);
             }
-            int a_max = ArgMax(Q);
-            real Q_max = Q[a_max];
+            //int a_max = ArgMax(Q);
+            //real Q_max = Q[a_max];
             real V = policy_evaluation.getValue(s);
-            printf ("%+.1f ", V);
-        }
-        printf ("\n");
-    }
-
-
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {
-            int s= x + y*width;
-            for (int a=0; a<n_actions; a++) {
-                Q[a] = policy_evaluation.getValue(s,a);
-            }
-            int a_max = ArgMax(Q);
-	    switch (a_max) {
-	    case Gridworld::NORTH: printf ("^"); break;
-	    case Gridworld::SOUTH: printf ("v"); break;
-	    case Gridworld::EAST: printf (">"); break;
-	    case Gridworld::WEST: printf ("<"); break;
+	    Gridworld::MapElement element = grid_world.whatIs(x, y);
+	    if (x) printf ("&");
+	    if (element == Gridworld::WALL) {
+		printf ("\\bsqr ");
+	    } else {
+		printf ("%+.1f ", V);
 	    }
         }
-        printf ("\n");
+        printf ("\\\\\\hline\n");
+    }
+
+
+    for (uint y=0; y<height; y++) {
+        for (uint x=0; x<width; x++) {
+	    int s = grid_world.getState(x, y);
+	    Gridworld::MapElement element = grid_world.whatIs(x, y);
+		  if (x) printf ("&");
+
+	    if (element == Gridworld::WALL) {
+		//printf ("#");
+		printf ("\\msqr ");
+	    } else {
+		for (int a=0; a<n_actions; a++) {
+		    Q[a] = policy_evaluation.getValue(s,a);
+		}
+		int a_max = ArgMax(Q);
+		switch (a_max) {
+		case Gridworld::NORTH: printf ("$\\uparrow$ "); break;
+		case Gridworld::SOUTH: printf ("$\\downarrow$ "); break;
+		case Gridworld::EAST:  printf ("$\\rightarrow$ "); break;
+		case Gridworld::WEST:  printf ("$\\leftarrow$ "); break;
+		}
+	    }
+        }
+        printf ("\\\\\\hline\n");
+        //printf ("\n");
     }
     policy_evaluation.ComputeStateValues(0.001, 1);
+    policy_evaluation.ComputeStateActionValues(0.001, 1);
     }
 #endif
     grid_world.Show();
