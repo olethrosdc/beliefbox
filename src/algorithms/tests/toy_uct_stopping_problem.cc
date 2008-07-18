30d29f7d3d3dc55c272b2cf0a4bcbaa53523732d
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-07-18 15:17:34 +0000

    Probably finished with the main class.

--- src/algorithms/tests/toy_uct_stopping_problem.cc
+++ src/algorithms/tests/toy_uct_stopping_problem.cc
@@ -15,7 +15,51 @@
 #include "Random.h"
 
 #include <list>
+#include <vector>
+#include <set>
 
+class SimpleBelief
+{
+protected:
+    BetaDistribution prior;
+    real r1;
+    real r2;
+public:
+    /// Create a belief
+    SimpleBelief(real alpha, real beta, real r1_, real r2_)
+        : prior(alpha, beta), r1(r1_), r2(r2_)
+    {
+    }
+
+    void update(int state, int action, real reward, int next_state)
+    {
+        if (state==0 && action == 0) {
+            if (reward == r1) {
+                prior.calculatePosterior(0.0);
+            } else if (reward == r2) {
+                prior.calculatePosterior(1.0);
+            } else {
+                fprintf (stderr, "Reward of %f should not have been observed in this state\n", reward);
+            }
+        }
+    }
+
+    real getProbability(int state, int action, real reward, int next_state)
+    {
+        if (state==0 && action == 0) {
+            real p = prior.getMean();
+            if (reward == r1) {
+                return 1.0 - p;
+            } else if (reward == r2) {
+                return p;
+            } else {
+                fprintf (stderr, "Reward of %f is illegal for s,a=0,0\n", reward);
+            }
+        }
+        return 1.0;
+    }
+    
+};
 
 template <typename B>
 class BeliefTree
@@ -35,27 +79,65 @@ public:
     public:
         Node& src;
         Node& dst;
+        real r;
+        real p;
     };
 
-    std::vector<Node> N;
-    std::vector<Edge> E;
+    std::vector<Node> nodes;
+    std::vector<Edge> edges;
+    
+    int n_states;
+    int n_actions;
+    BeliefTree(int n_states_, int n_actions_)
+        : n_states(n_states_), n_actions(n_actions_)
+    {
+    }
     /// Return 
-    Node& Expand(int i, int a, real r, int s)
+    Node& ExpandAction(int i, int a, real r, int s)
     {
         Node next;
-        next.belief = N[i].belief;
+        next.belief = nodes[i].belief;
         next.state = s;
-        next.belief.update(N[i].s, a, r, s);
-        Node& node_ref = N.push_back(next);
+        real p = nodes[i].belief.getProbability(nodes[i].s, a, r, s);
+        next.belief.update(nodes[i].s, a, r, s);
+        Node& node_ref = nodes.push_back(next);
         Edge edge;
-        edge.src = N[i];
+        edge.src = nodes[i];
         edge.dst = node_ref;
-        Edge edge_ref=  E.push_back(edge);
-        N[i].edges.push_back(edge_ref);
-
+        edge.p = p;
+        Edge edge_ref=  edges.push_back(edge);
+        nodes[i].edges.push_back(edge_ref);
         return node_ref;
     }
-    
+
+    /// Return 
+    std::set<Node&> Expand(int i)
+    {
+        B belief = nodes[i].belief;
+        int state = nodes[i].state;
+        std::set<Node&> node_set;
+
+        if (state==1) { 
+            // terminal state, all actions do nothing whatsoever.            
+            node_set.push_back(ExpandAction(i, 0, 0.0, 1));
+            node_set.push_back(ExpandAction(i, 1, 0.0, 1));
+        } else if (state==0) {
+            // If we play, there are two possibilities
+            node_set.push_back(ExpandAction(i, 0, 1.0, 0));
+            node_set.push_back(ExpandAction(i, 0, -1.0, 0));
+            // if we move to the terminal state nothing happens
+            node_set.push_back(ExpandAction(i, 1, 0.0, 1));
+        }
+        return node_set;
+    }
+
+
+    DiscreteMDP CreateMDP()
+    {
+        int current_node = nodes[0];
+        int n_nodes = nodes.size();
+        DiscreteMDP (n_nodes, 2, NULL);
+    }
 };
 
 
@@ -72,12 +154,9 @@ int main (int argc, char** argv)
 {
     real alpha = 0;
     real beta = 0;
-    //ConjugatePrior* prior = new BetaDistribution(alpha, beta);
-
-    real mean_r = urandom(-1, 1);
+    real actual_probability = urandom(0, 1);
     
-    //EvaluateAlgorithm(algorithm, mean_r);
-    
+        
     return 0;
 }
 
