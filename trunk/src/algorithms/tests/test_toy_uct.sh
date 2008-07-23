3fb63d3bf8cea48713d31eaa0ca383a28f126b3b
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-07-23 16:26:16 +0000

    Blah

--- trunk/src/algorithms/tests/test_toy_uct.sh
+++ trunk/src/algorithms/tests/test_toy_uct.sh
@@ -1,8 +1,11 @@
+#! /bin/bash
 
-for gamma in 0.9 0.95 0.99 0.995 0.999
+rm -f results
+for gamma in 0.9 0.91 0.925 0.95 0.99 0.995 0.999
 do
     for iter in 2 3 4 5 6 7 8 9 10
     do
         time ./bin/toy_uct_stopping_problem $gamma $iter 0 >>results
+        tail -n 1 results
     done
 done
