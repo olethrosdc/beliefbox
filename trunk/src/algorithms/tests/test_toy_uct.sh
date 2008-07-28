fb4a06665c475558f3f1353d0230d59012239326
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-07-28 08:15:31 +0000

    Resolved.

--- trunk/src/algorithms/tests/test_toy_uct.sh
+++ trunk/src/algorithms/tests/test_toy_uct.sh
@@ -5,7 +5,7 @@ for gamma in  0.99999 #0.1 0.5 0.9 0.99 0.999 0.9999
 do
     for iter in  1 2 3 4 #64 #1 2 3 4 5 6 7 8 9 10 11 12 16 32 64
     do
-        time ./bin/toy_uct_stopping_problem $gamma $iter 0 >>results
+        time ./bin/toy_uct_stopping_problem $gamma $iter 0 10000 >>results
         tail -n 1 results
     done
 done
