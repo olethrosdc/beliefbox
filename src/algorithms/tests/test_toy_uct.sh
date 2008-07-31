67ac949149ca9295e4108640b5e909c372a8ffdc
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-07-31 07:52:23 +0000

    Test stuff.

--- src/algorithms/tests/test_toy_uct.sh
+++ src/algorithms/tests/test_toy_uct.sh
@@ -3,8 +3,9 @@
 
 for iter in  1 2 3 4 6 8 10 12 16 32 64 128
 do
-    results=results/bfs${iter}.out
+    results=results/bfs_slow_${iter}.out
     mkdir -p results
-    qsub -v"iter=$iter","results=$results" ./test_toy_uct_sub.sh
+    rm -f $results
+    bash ./test_toy_uct_sub2.sh $iter $results &
 done
 
