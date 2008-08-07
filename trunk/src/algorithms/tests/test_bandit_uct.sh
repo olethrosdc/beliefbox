d78c501053817d7034e14ad1c3fdcc625953d9d4
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-08-07 17:06:59 +0000

    Fixed conflict.

--- trunk/src/algorithms/tests/test_bandit_uct.sh
+++ trunk/src/algorithms/tests/test_bandit_uct.sh
@@ -8,18 +8,16 @@ echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" >
 
 for actions in 2 4 8
 do
-    for iter in  1 2 3 4 5 6 7 8 10 12 14 18 22
+    for gamma in 0.1 0.5 0.9 0.99 0.999
     do
-        for method in 0 1 2 3 4 5 6 7
+        for iter in  1 2 3 4 5 6 7 8 10 12 14 18 22
         do
-            for gamma in 0.1 0.5 0.9 0.99 0.999
+            for method in 0 1 2 3 4 5 6 7
             do
                 ./bin/bandit_uct $method $gamma $iter $actions 0 $experiments >>$results
-            tail -n 1 $results
+                tail -n 1 $results
             done
             
         done
     done
 done
-
-
