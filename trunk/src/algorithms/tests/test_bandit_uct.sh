0ee6069ed94acd297e9df66edc24f60c3b68c70c
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-08-16 10:56:55 +0000

    Fixed problems with scripts.

--- trunk/src/algorithms/tests/test_bandit_uct.sh
+++ trunk/src/algorithms/tests/test_bandit_uct.sh
@@ -2,22 +2,21 @@
 #
 
 
-results=results/belief_uct/bandit/uct2.out
+results=results/belief_uct/bandit/uct_complete.out
 experiments=1000
 ##echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" > $results
 
-for actions in 2 4 8
+for actions in 2 4 #8
 do
-    for iter in  2 3 4 5 6 7 8 10 12 14 18 22
+    for iter in  0 1 2 3 4 5 6 7 8 10 12 14 18 22 28 32 64
     do
-        for gamma in 0.1 0.5 0.9 0.99 0.999
-        do
-            for method in 12 #8 9 10 11 # 0 1 2 3 4 5 6 7
+        for method in 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+	    do
+	        #qsub -v"method=$method","iter=$iter","actions=$actions","experiments=$experiments","results=$results"  ./test_bandit_uct_sub.sh
+            for gamma in  0.5 0.9 0.99 0.999 0.9999 0.99999
             do
-                ./bin/bandit_uct $method $gamma $iter $actions 0 $experiments  >>$results
-                tail -n 1 $results
+                time ./bin/bandit_uct $method $gamma $iter $actions 0 $experiments  >>$results 
             done
-            
         done
     done
 done
