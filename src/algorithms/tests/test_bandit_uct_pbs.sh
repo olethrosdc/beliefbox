3efa9795a377397b17bcdcdcdf757e01f6b810aa
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-08-16 10:54:54 +0000

    Made it work with PBS

--- src/algorithms/tests/test_bandit_uct_pbs.sh
+++ src/algorithms/tests/test_bandit_uct_pbs.sh
@@ -2,17 +2,18 @@
 #
 
 
-results=results/belief_uct/bandit/uct_complete.out
-experiments=1000
-##echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" > $results
 
-for actions in 2 4 #8
-do
-    for iter in  0 1 2 3 4 5 6 7 8 10 12 14 18 22 28 32 64
+#echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" > $results
+experiments=1000
+
+for gamma in  0.5 #0.9 0.99 0.999 0.9999 0.99999
+  do
+  for iter in  0 1 #2 3 4 5 6 7 8 10 12 14 18 22 28 32 64
     do
-      for method in 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-	do
-	qsub -v"method=$method","iter=$iter","actions=$actions","experiments=$experiments","results=$results"  ./test_bandit_uct_sub.sh
-        done
+    for method in 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+      do
+      results=results/belief_uct/bandit/g${gamma}_m${method}_i${iter}_uct_complete.out
+      qsub -v"method=$method","iter=$iter","gamma=$gamma","experiments=$experiments","results=$results"  ./test_bandit_uct_sub.sh
     done
+  done
 done
