65a34a7b32e062c66b755caaa7526343db76553e
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-08-22 07:59:46 +0000

    Fixed conflict, added MDP thingies.

--- src/algorithms/tests/test_bandit_uct.m
+++ src/algorithms/tests/test_bandit_uct.m
@@ -2,29 +2,34 @@
 
 lw=4;
 
-X=load("results/belief_uct/bandit/uct2.out");
+X=load("results/belief_uct/bandit/uct_complete.out");
 Y=load("results/belief_uct/bandit/ucb.out");
-gamma = 0.9;
+gamma = 0.999;
 n_arms=4;
+disc = 0;
 hold off;
-idx = Y(:,2)==gamma & Y(:,1)==0 & Y(:,3)== n_arms; plot([1 22], [1 1]*(Y(idx,6)-Y(idx,4)), "0@-;ucb;", "linewidth", lw);
+idx = Y(:,2)==gamma & Y(:,1)==0 & Y(:,3)== n_arms;
+plot([1 32], [1 1]*(Y(idx, 6 + disc) - Y(idx,4 + disc)), "0@-;ucb;", "linewidth", lw);
 hold on;
-uct=5;
-oracle=7;
+uct=5 + disc;
+oracle=7 + disc;
 sel_exp = X(:,3)==gamma & X(:,4)==n_arms;
 
 idx = sel_exp & X(:,1)==0; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "1-;serial;", "linewidth", lw);
 idx = sel_exp & X(:,1)==1; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "1@-;random;", "linewidth", lw);
-idx = sel_exp & X(:,1)==2; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "2-;mean;", "linewidth", lw);
-idx = sel_exp & X(:,1)==3; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "2@-;mean gamma;", "linewidth", lw);
-idx = sel_exp & X(:,1)==4; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "3-;thompson;", "linewidth", lw);
-idx = sel_exp & X(:,1)==5; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "3@-;thompson gamma;", "linewidth", lw);
-idx = sel_exp & X(:,1)==12; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "5@-;high prob;", "linewidth", lw);
-idx = sel_exp & X(:,1)==9; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "4@-;high prob gamma;", "linewidth", lw);
+idx = sel_exp & X(:,1)==2; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "2-;LB;", "linewidth", lw);
+#idx = sel_exp & X(:,1)==3; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "2@-;LB gamma;", "linewidth", lw);
+idx = sel_exp & X(:,1)==6; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "3-;thompson;", "linewidth", lw);
+idx = sel_exp & X(:,1)==7; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "3@-;thompson gamma;", "linewidth", lw);
+idx = sel_exp & X(:,1)==8; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "4-;hp UB;", "linewidth", lw);
+idx = sel_exp & X(:,1)==9; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "4@-;hp UB gamma;", "linewidth", lw);
+idx = sel_exp & X(:,1)==12; plot(X(idx,2), X(idx,oracle)-X(idx,uct), "5-;mean UB;", "linewidth", lw);
+idx = sel_exp & X(:,1)==13; semilogy(X(idx,2), X(idx,oracle)-X(idx,uct), "5@-;mean UB gamma;", "linewidth", lw);
 
 xlabel("number of expansions");
 ylabel("total regret");
-legend("location", "northeast");
+title("n=4, gamma=0.999");
+legend("location", "southwest");
 legend("boxon");
 grid on
 print("test_bandit_uct.eps");
