86e77d9f1c19112b1b1b42f56cec68405b2de71e
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2008-10-22 10:01:13 +0000

    Difffs fixed

--- trunk/src/algorithms/tests/scripts/quick_bandit_test.sh
+++ trunk/src/algorithms/tests/scripts/quick_bandit_test.sh
@@ -1,9 +1,9 @@
-experiments=1000
+experiments=100
 horizon=100000
 method=$1
 for gamma in 0.5 0.9 0.99 0.999 0.9999 0.99999
 do
-    for iter in 0 1 2 3 4 5 8 #6 7 8 9 10 11 12
+    for iter in 8 10 12 24 #0 1 2 3 4 5 8 #6 7 8 9 10 11 12
     do
         ./bin/bandit_uct $method $gamma $iter 2 0 $experiments >>uct_bnb.out
         tail uct_bnb.out
