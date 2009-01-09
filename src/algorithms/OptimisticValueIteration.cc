fcef0eea4214e36c7dfb877369ed92a3efade3df
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-01-09 17:35:13 +0000

    Added first version for dirichlet parameters etc.

---
+++ src/algorithms/OptimisticValueIteration.cc
@@ -0,0 +1,146 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2006 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Id: ValueIteration.c,v 1.5 2006/11/08 17:20:17 cdimitrakakis Exp cdimitrakakis $
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "OptimisticValueIteration.h"
+#include "real.h"
+#include "MathFunctions.h"
+#include "Vector.h"
+#include <cmath>
+#include <cassert>
+
+OptimisticValueIteration::OptimisticValueIteration(const DiscreteMDP* mdp, real gamma, real baseline)
+{
+    assert (mdp);
+    assert (gamma>=0 && gamma <=1);
+    this->mdp = mdp;
+    this->gamma = gamma;
+    this->baseline = baseline;
+    n_actions = mdp->GetNActions();
+    n_states = mdp->GetNStates();
+    Reset();
+}
+
+void OptimisticValueIteration::Reset()
+{
+    int N = n_states * n_actions;
+    V.resize(n_states);
+    dV.resize(n_states);
+    pV.resize(n_states);
+    Q.resize(n_states);
+    Q_data.resize(N);
+    dQ.resize(n_states);
+    dQ_data.resize(N);
+    pQ.resize(n_states);
+    pQ_data.resize(N);
+    for (int s=0; s<n_states; s++) {
+        V[s] = 0.0;
+        dV[s] = 0.0;
+        pV[s] = 0.0;
+        Q[s] = &Q_data[s*n_actions];
+        dQ[s] = &dQ_data[s*n_actions];
+        pQ[s] = &pQ_data[s*n_actions];
+        for (int a=0; a<n_actions; a++) {
+            Q[s][a] = 0.0;
+            dQ[s][a] = 0.0;
+            pQ[s][a] = 0.0;
+        }
+    }
+}
+
+OptimisticValueIteration::~OptimisticValueIteration()
+{
+}
+
+void OptimisticValueIteration::ComputeStateValues(real epsilon, real threshold, int max_iter)
+{
+    do {
+        Delta = 0.0;
+        for (int s=0; s<n_states; s++) {
+                //real v = V[s];
+            real Q_a_max = -RAND_MAX;
+            int a_max = 0;
+            for (int a=0; a<n_actions; a++) {
+                real S = 0.0;
+                DiscreteStateSet next = mdp->getNextStates(s, a);
+                for (DiscreteStateSet::iterator i=next.begin();
+                     i!=next.end();
+                     ++i) {
+                    int s2 = *i;
+                    real P = mdp->getTransitionProbability(s, a, s2);
+                    real r = 
+                    real R = mdp->getExpectedReward(s, a) + gamma * V[s2] - baseline;
+                    S += P * R;
+                }
+                if (a==0 || Q_a_max < S) {
+                    a_max = a;
+                    Q_a_max = S;
+                }
+            }
+            V[s] = Q_a_max;
+            dV[s] = pV[s] - V[s];
+            pV[s] = V[s];
+        }
+        Delta = Max(dV) - Min(dV);
+        max_iter--;
+
+    } while(Delta >= threshold && max_iter > 0);
+	
+}
+
+
+/** ComputeStateActionValues
+   
+    threshold - exit when difference in Q is smaller than the threshold
+    max_iter - exit when the number of iterations reaches max_iter
+
+*/
+
+void OptimisticValueIteration::ComputeStateActionValues(real threshold, int max_iter)
+{
+    int N = n_states * n_actions;
+
+    for (int s=0; s<n_states; s++) {
+        for (int a=0; a<n_actions; a++) {
+            dQ[s][a] = 0.0;
+        }
+    }
+    do {
+        Delta = 0.0;
+        for (int s0=0; s0<n_states; s0++) {
+            int s = s0;
+            for (int a=0; a<n_actions; a++) {
+                real sum = 0.0;
+
+                DiscreteStateSet next = mdp->getNextStates(s, a);
+                for (DiscreteStateSet::iterator i=next.begin();
+                     i!=next.end();
+                     ++i) {
+                    int s2 = *i;
+                    real P = mdp->getTransitionProbability(s, a, s2);
+                        //if (P > 0) {
+                        real R = mdp->getExpectedReward(s, a) - baseline;
+                        real Q_a_max = Max(n_actions, Q[s2]);
+                        sum += P*(R + gamma*Q_a_max);
+                            //}
+
+                }
+                Q[s][a] = sum;
+                dQ[s][a] = pQ[s][a] - sum;
+                pQ[s][a] = sum;
+            }
+        }
+        
+        Delta = Max(N, &dQ_data[0]) - Min(N, &dQ_data[0]);			
+        max_iter--;
+    } while(Delta >= threshold && max_iter > 0);
+}
+
---
+++ src/models/DiscreteMDPCounts.h
@@ -0,0 +1,44 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2005-2007 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DISCRETE_MDP_COUNTS
+#define DISCRETE_MDP_COUNTS
+
+#include "MDPModel.h"
+#incldue "Dirichlet.h"
+
+class DiscreteMDPCounts : public MDPModel
+{
+protected:
+    std::vector<DirichletDistribution> P;
+    int N;
+    int getID (int s, int a)
+    {
+        SMART_ASSERT(s>=0 && s<n_states)(s)(n_states);
+        SMART_ASSERT(a>=0 && a<n_actions)(a)(n_actions);
+        return s*n_actions + a;
+    }
+public:
+    DiscreteMDPCounts (int n_states, int n_actions, int init_transition_count=0, int init_reward_count = 0, real init_reward = 0.0);
+    virtual ~DiscreteMDPCounts();
+    virtual void AddTransition(int s, int a, real r, int s2);
+    void ShowModel();
+    virtual real GenerateReward (int s, int a);
+    virtual int GenerateTransition (int s, int a);
+    virtual real getTransitionProbability (int s, int a, int s2);
+    virtual real getExpectedReward (int s, int a);
+    virtual int getNStates () { return N;}
+    virtual void Reset();
+};
+
+
+#endif
