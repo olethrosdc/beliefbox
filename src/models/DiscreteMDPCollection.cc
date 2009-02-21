51931e52b171f65253339f7171de5f51a96f00a0
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-02-21 18:57:49 +0000

    Added discrete mdp data collection

---
+++ src/models/DiscreteMDPCollection.cc
@@ -0,0 +1,77 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "DiscreteMDPCollection.h"
+#include "MultinomialDistribution.h"
+
+DiscreteMDPCollection::DiscreteMDPCollection(int n_aggregates, int n_states, int n_actions) : MDPModel(n_states, n_actions)
+{
+    P.resize(n_aggregates);
+    for (int i=0; i<n_aggregates; i++) {
+        P[i] = 1.0 / (real) n_aggregates;
+        if (i==0) {
+            A[i] = new DiscreteMDPCounts(n_states, n_actions);
+        } else {
+            int n = floor(log(n_states) / log(2));
+            // make sure 1 < n_sets < n_states
+            int n_sets = n_states >> (((i-1) % n) + 1);
+            printf ("Collection size: %d / %d\n", n_sets, n_states);
+            A[i] = new DiscreteMDPAggregate(n_states, n_sets, n_actions);
+        }
+    }
+}
+
+DiscreteMDPCollection::~DiscreteMDPCollection()
+{
+    for (uint i=0; i<A.size(); i++) {
+        delete A[i];
+    }
+}
+
+void DiscreteMDPCollection::AddTransition(int s, int a, real r, int s2)
+{
+    for (uint i=0; i<A.size(); ++i) {
+        A[i]->AddTransition(s, a, r, s2);
+    }
+}
+real DiscreteMDPCollection::GenerateReward (int s, int a) const
+{
+    int i = DiscreteDistribution::generate(P);
+    return A[i]->GenerateReward(s, a);
+}
+int DiscreteMDPCollection::GenerateTransition (int s, int a) const
+{
+    int i = DiscreteDistribution::generate(P);
+    return A[i]->GenerateTransition(s, a);
+}
+real DiscreteMDPCollection::getTransitionProbability (int s, int a, int s2) const
+{
+    real p = 0.0;
+    for (uint i=0; i<A.size(); ++i) {
+        p += P[i]*A[i]->getTransitionProbability(s, a, s2);
+    }
+    return p;
+}
+real DiscreteMDPCollection::getExpectedReward (int s, int a) const
+{
+    real E = 0.0;
+    for (uint i=0; i<A.size(); ++i) {
+        E += P[i]*A[i]->getExpectedReward(s, a);
+    }
+    return E; 
+}
+void DiscreteMDPCollection::Reset()
+{
+      for (uint i=0; i<A.size(); ++i) {
+          A[i]->Reset();
+      }  
+}
---
+++ src/models/DiscreteMDPCollection.h
@@ -0,0 +1,40 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DISCRETE_MDP_COLLECTION
+#define DISCRETE_MDP_COLLECTION
+
+#include "DiscreteMDPCounts.h"
+#include "DiscreteMDPAggregate.h"
+#include <set>
+#include <vector>
+
+/// blah
+class DiscreteMDPCollection : MDPModel
+{
+protected:
+    std::vector<DiscreteMDPCounts*> A;
+    std::vector<real> P;
+public:
+    DiscreteMDPCollection(int n_aggregates, int n_states, int n_actions);
+    virtual ~DiscreteMDPCollection();
+    virtual void AddTransition(int s, int a, real r, int s2);
+    virtual real GenerateReward (int s, int a) const;
+    virtual int GenerateTransition (int s, int a) const;
+    virtual real getTransitionProbability (int s, int a, int s2) const;
+    virtual real getExpectedReward (int s, int a) const;
+    virtual void Reset();
+};
+
+
+
+#endif
