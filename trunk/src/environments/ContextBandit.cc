2d8aba5714b34292db60d04161f4e88a754035b3
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-03-25 08:07:15 +0000

    factorised

--- trunk/src/environments/ContextBandit.cc
+++ trunk/src/environments/ContextBandit.cc
@@ -12,6 +12,7 @@
 
 #include "ContextBandit.h"
 
+
 ContextBandit::ContextBandit(uint n_actions_,
                              uint n_features,
                              unit values_per_feature,
@@ -32,8 +33,43 @@ void RandomMDP::Reset()
 /// returns true if the action succeeds, false if we are in a terminal state
 bool RandomMDP::Act(int action)
 {
-    reward = mdp->generateReward(state, action);
-    state = mdp->generateState(state, action);
+    int f0 = state & 7;
+    int f1 = (state >> 3) & 7;
+    int f2 = (state >> 6) & 7;
+   	real mean;
+	real sigma = 2;
+
+	if ((f1 % 4) == 1)
+	{
+		if (f2 % 2)
+		{
+			mean = 1;
+		}
+		else
+		{
+			mean = -1;
+		}
+	}
+	else
+	{
+		if (f0 % 2)
+		{
+			mean = 1;
+		}
+		else
+		{
+			mean = -1;
+		}
+	}
+	if (action)   // a = 0 or a = 1
+	{
+		mean = -mean;
+	}
+
+    state = (int) rng->discrete_uniform(n_states);
+    normal.setVariance(sigma*sigma);
+    normal.setMean(mean);
+    reward = normal.generate();
     //reward = mdp->getExpectedReward(state, action);
     if (state == terminal_state) {
         return false; // terminated
