a6806f06c7ec7c61eab3f00c9142d30c14735d99
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-04-17 14:53:30 +0000

    experiments for SARA

--- trunk/src/algorithms/tests/scripts/test_bandit_uct.sh
+++ trunk/src/algorithms/tests/scripts/test_bandit_uct.sh
@@ -2,19 +2,21 @@
 #
 
 
-results=results/belief_uct/bandit/uct_complete.out
+results=results/belief_uct/bandit/uct_is.out
 experiments=1000
-##echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" > $results
+#echo "#meth #n_iter #gamma #actions #total #discounted #o_total #o_discounted" > $results
 
+#rm -f $results
 for actions in 2 4 #8
 do
-    for iter in  0 1 2 3 4 5 6 7 8 10 12 14 18 22 28 32 64
+    for gamma in  0.5 0.9 0.99 0.999 0.9999 0.99999
     do
-        for method in 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-	    do
+        
+        for iter in  1 2 3 4 5 6 7 8
+        do
+            for method in 0 1 4 5 13 15 16 17
+	        do
 	        #qsub -v"method=$method","iter=$iter","actions=$actions","experiments=$experiments","results=$results"  ./test_bandit_uct_sub.sh
-            for gamma in  0.5 0.9 0.99 0.999 0.9999 0.99999
-            do
                 time ./bin/bandit_uct $method $gamma $iter $actions 0 $experiments  >>$results 
             done
         done
