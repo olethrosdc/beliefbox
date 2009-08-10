aa922ea9b3d1bed02f02279b596df366e53d6592
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-08-10 19:42:39 +0000

    added script and stuf

---
+++ src/statistics/tests/bayesian_markov_chain_text.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+outdir=~/experiments/bpsr/alice/
+mkdir -p $outdir
+rm -f $outdir/summary.err
+horizon=
+for (( i=0; i<=16; i++ ))
+do
+    ./bin/bayesian_markov_chain_text ./data/alice.dat $i $horizon | grep Err  >>$outdir/summary.err
+    mv bmc_text.error $outdir/bmc${i}.err
+    mv bpsr_text.error $outdir/bpsr${i}.err
+    mv ctw_text.error $outdir/ctw${i}.err
+done
+
