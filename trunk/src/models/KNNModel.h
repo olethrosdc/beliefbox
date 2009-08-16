c882a4c278a8769c599f80cb191f0abb396e908e
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-08-16 22:50:50 +0000

    integrated value iteration

--- trunk/src/models/KNNModel.h
+++ trunk/src/models/KNNModel.h
@@ -24,8 +24,9 @@ public:
     int a;
     real r;
     Vector s2;
+    real V;
     TrajectorySample(Vector s_, int a_, real r_, Vector s2_)
-        : s(s_), a(a_), r(r_), s2(s2_)
+        : s(s_), a(a_), r(r_), s2(s2_), V(0.0)
     {
     }
         
@@ -40,12 +41,18 @@ protected:
     std::vector<KDTree<TrajectorySample>*> kd_tree;
     //RBFBasisSet basis;
     std::list<TrajectorySample> samples;
+    real gamma;
 public:	
-    KNNModel(int n_actions, int n_dim);
+    KNNModel(int n_actions, int n_dim, real gamma_ = 0.9);
     ~KNNModel();
     void AddSample(TrajectorySample x);
     void GetExpectedTransition(Vector& x, int a, real& r, Vector& y, int K, real b);
     void GetExpectedTransitionDiff(Vector& x, int a, real& r, Vector& y, int K, real b);
+    real GetExpectedActionValue(Vector& x, int a, int K, real b);
+    real GetExpectedValue(Vector& x, int K, real b);
+    int GetBestAction(Vector& x, int K, real b);
+    void UpdateValue(TrajectorySample& start_sample, int K, real b);
+    void ValueIteration(int K, real b);
 };
 
 
