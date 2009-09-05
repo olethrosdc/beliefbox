45cb1bd173237e3324240e2de70da0d53b28598d
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-09-05 08:37:10 +0000

    added model collection

---
+++ trunk/src/algorithms/ModelCollectionRL.cc
@@ -0,0 +1,96 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "ModelCollectionRL.h"
+
+ModelCollectionRL::ModelCollectionRL(int n_states_,
+                                     int n_actions_,
+                                     real gamma_,
+                                     real epsilon_,
+                                     DiscreteMDPCollection* model_,
+                                     bool use_value_iteration_) :
+    ModelBasedRL(n_states_, n_actions_, gamma_, epsilon_, model_, use_value_iteration_),
+    collection(model_),
+    n_models(collection->get_n_models()),
+    models(collection->GetModels())
+{
+    mdp_vector.resize(n_models);
+    vi_vector.resize(n_models);
+    for (int i = 0; i < n_models; ++i) {
+        mdp_vector[i] = models[i]->CreateMDP();
+        vi_vector[i] = new ValueIteration(mdp, gamma);
+    }
+}
+
+ModelCollectionRL::~ModelCollectionRL()
+{
+    for (int i = 0; i < n_models; ++i) {
+        delete vi_vector[i];
+        delete mdp_vector[i];
+    }
+}
+
+//void ModelCollectionRL::Reset()
+//{
+//    state = -1;
+//    model->Reset();
+//}
+
+/// Get an action using the current exploration policy.
+/// it calls Observe as a side-effect.
+int ModelCollectionRL::Act(real reward, int next_state)
+{
+    assert(next_state >= 0 && next_state < n_states);
+
+    // update the model
+    if (state >= 0) {
+        model->AddTransition(state, action, reward, next_state);
+    }
+    state = next_state;
+
+    std::vector<real>& P  = collection->GetModelProbabilities();
+    std::vector<DiscreteMDPCounts*>& M  = collection->GetModels();
+    for (int a = 0; a < n_actions; ++a) {
+        tmpQ[a] = 0.0;
+    }
+    
+    for (int i = 0; i < n_models; ++i) {
+        // re-create the MDP
+        if (mdp_vector[i]) {
+            delete mdp_vector[i];
+            mdp_vector[i] = NULL;
+        }
+        mdp_vector[i] = M[i]->CreateMDP();
+        
+        // update values
+        vi_vector[i]->mdp = mdp;
+        vi_vector[i]->ComputeStateActionValues(0.00, 1);
+        for (int a=0; a<n_actions; a++) {
+            tmpQ[a] += P[i] * value_iteration->getValue(next_state, a);
+        }
+    }
+
+    // choose action
+    int next_action;
+    if (urandom()<epsilon) {
+        next_action = (int) floor(urandom(0.0, n_actions));
+        //printf ("\n");
+    } else {
+        next_action = ArgMax(tmpQ);
+        //printf (" * A = %d\n", next_action);
+    }
+    action = next_action;
+
+    return action;
+}
+
+
---
+++ trunk/src/algorithms/ModelCollectionRL.h
@@ -0,0 +1,70 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MODEL_COLLECTION_RL_H
+#define MODEL_COLLECTION_RL_H
+
+#include "DiscreteMDP.h"
+#include "DiscretePolicy.h"
+#include "ExplorationPolicy.h"
+#include "Matrix.h"
+#include "real.h"
+#include "OnlineAlgorithm.h"
+#include "DiscreteMDPCollection.h"
+#include "ValueIteration.h"
+#include "ModelBasedRL.h"
+
+#include <vector>
+
+class ModelCollectionRL : public ModelBasedRL
+{
+protected:
+    std::vector<DiscreteMDP*> mdp_vector;
+    std::vector<ValueIteration*> vi_vector;
+    DiscreteMDPCollection* collection;
+    int n_models;
+    std::vector<DiscreteMDPCounts*> & models;
+public:
+    ModelCollectionRL(int n_states_,
+                 int n_actions_,
+                 real gamma_,
+                 real epsilon_,
+                 DiscreteMDPCollection* model_,
+                 bool use_value_iteration_ = true);
+    virtual ~ModelCollectionRL();
+    //virtual void Reset();
+    /// Full observation
+    //virtual real Observe (int state, int action, real reward, int next_state, int next_action);
+    /// Partial observation 
+    //virtual real Observe (real reward, int next_state, int next_action);
+    /// Get an action using the current exploration policy.
+    /// it calls Observe as a side-effect.
+    virtual int Act(real reward, int next_state);
+
+    virtual real getValue (int state, int action)
+    { 
+        std::vector<real>& P  = collection->GetModelProbabilities();
+        if (use_value_iteration) {
+            real Q = 0.0;
+            for (int i=0; i<n_models; ++i) {
+                Q += P[i] * value_iteration->getValue(state, action);
+            }
+            return Q;
+        } else {
+            return 0.0;
+        }
+    }
+    
+};
+
+#endif
+
