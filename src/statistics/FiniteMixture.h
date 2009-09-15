6aa53c256e9fb3af3bec412b8c5c129ed25c813e
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-09-15 08:16:17 +0000

    added finite mixture

---
+++ src/statistics/FiniteMixture.h
@@ -0,0 +1,23 @@
+/* -*- Mode: c++;  -*- */
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef FINITE_MIXTURE_H
+#define FINITE_MIXTURE_H
+
+/**
+   \ingroup StatisticsGroup
+ */
+/*@{*/
+
+// a finite mixture template
+
+/*@}*/
+
+#endif
