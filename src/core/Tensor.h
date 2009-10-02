ee90ab620182f98538cff92a71da7644e8787239
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-10-02 06:48:37 +0000

    added tensor

---
+++ src/core/Tensor.h
@@ -0,0 +1,69 @@
+/* -*- Mode: c++ -*- */
+// copyright (c) 2009 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include <vector>
+#include <cstdlib>
+#include <cassert>
+
+#include "real.h"
+
+class Tensor
+{
+protected:
+    std::vector<int> X;
+    real* data;
+    int n;
+    long K;
+public:
+    Tensor(std::vector<int>& X_) : X(X_), n(X.size())
+    {
+        K = 1;
+        for (int i=0; i<n; ++i) {
+            if (X[i] == 0) {
+                fprintf(stderr, "Tensor.h: Cannot allocate a dimension of size zero\n");
+                exit(-1);
+            }
+            K *= X[i];
+        }
+        data = (real*) calloc(K, sizeof(real));
+    }
+
+    ~Tensor()
+    {
+        free(data);
+    }
+
+    // get value
+    real& Y(std::vector<int>& x)
+    {
+        assert((int) x.size() == n);
+        long index = 0;
+        long M = 1;
+        for (int i=0; i<n; ++i) {
+            assert(x[i] >= 0 && x[i] < X[i]);
+            index += (long) x[i] * M;
+            M *= X[i];
+        }
+        assert(index >= 0 && index < K);
+        return data[index];
+    }
+    void Reset()
+    {
+        for (long i=0; i<K; ++i) {
+            data[i] = 0;
+        }
+    }
+};
+
+#endif
