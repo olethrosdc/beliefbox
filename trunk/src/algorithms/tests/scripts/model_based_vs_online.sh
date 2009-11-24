0c06516158cd275653337344c35241000df8495f
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2009-11-24 16:13:38 +0000

    Streamlined scripts and algorithms for context bandit problem

--- trunk/src/algorithms/tests/scripts/model_based_vs_online.sh
+++ trunk/src/algorithms/tests/scripts/model_based_vs_online.sh
@@ -1,8 +1,8 @@
 n_runs=10000
-n_episodes=10
+n_episodes=1
 n_steps=10000
 
-gamma=0.999
+gamma=0.9
 lambda=0.9
 random=0.01
 
@@ -22,34 +22,10 @@ echo "Run parameters" >run.params
 echo  "exc n_states n_actions gamma lambda random n_runs n_episodes n_steps" >>run.params
 echo  ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps >>run.params
 
-time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps Sarsa $environment >sarsa.out
-grep MSE sarsa.out >sarsa.mse
-grep REWARD sarsa.out >sarsa.reward
-grep PAYOFF sarsa.out >sarsa.payoff
-
-time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps QLearning $environment >qlearning.out
-grep MSE qlearning.out >qlearning.mse
-grep REWARD qlearning.out >qlearning.reward
-grep PAYOFF qlearning.out >qlearning.payoff
-
-#time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps Model $environment >model.out
-#grep MSE model.out >model.mse
-#grep REWARD model.out >model.reward
-#grep PAYOFF model.out >model.payoff
-
-time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps ContextBanditGaussian $environment >model.out
-grep MSE model.out >model.mse
-grep REWARD model.out >model.reward
-grep PAYOFF model.out >model.payoff
-
-time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_#steps Aggregate $environment >aggregate.out
-grep MSE aggregate.out >aggregate.mse
-grep REWARD aggregate.out >aggregate.reward
-grep PAYOFF aggregate.out >aggregate.payoff
-
-
-
-time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps ContextBanditCollection $environment >collection.out
-grep MSE collection.out >collection.mse
-grep REWARD collection.out >collection.reward
-grep PAYOFF collection.out >collection.payoff
+for model in Sarsa QLearning ContextBanditGaussian Aggregate ContextBanditCollection
+do
+    time ${exc} $n_states $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps ${model} $environment >${model}.out
+    grep MSE ${model}.out >${model}.mse
+    grep REWARD ${model}.out >${model}.reward
+    grep PAYOFF ${model}.out >${model}.payoff
+done
