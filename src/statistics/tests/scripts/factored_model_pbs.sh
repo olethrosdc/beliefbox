929948648919f0b3ec33a3b6f868707ebf8a0c64
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-01-28 16:40:06 +0000

    added cluster scripts

--- src/statistics/tests/scripts/factored_model_pbs.sh
+++ src/statistics/tests/scripts/factored_model_pbs.sh
@@ -1,21 +1,22 @@
-#! /bin/sh
+#! /bin/bash
 
-for p in 0.0 0.01 0.1
+mkdir -p $HOME/experiments
+cd $HOME/experiments
+
+script_dir=$HOME/projects/beliefbox/src/statistics/tests/scripts
+environment=1DMaze
+
+for p in 0.0 #0.01 0.1
 do
-    outdir=~/experiments/bvmdp/1dmaze/1000iter/2obs/p${p}
-    mkdir -p $outdir
-    iter=1000
-    T=100000
-    echo "Params: $iter $T" >$outdir/params.txt
-    
-    for states in 4 8 10 12 16 32
-    do
-        for depth in 1 2 4 8 10 12
-        do
-            for model in FMC BFMC BVMM CTW
+    for ap in 1.0 #0.5 0.1
+    do 
+	out_dir=$HOME/results/bvmdp/${environment}/${p}p_${ap}ap/
+	for states in 4 #8 10 12 16 32
+	do
+            for depth in 1 #2 4 8 10 12
             do
-                echo "./bin/factored_model 1DMaze $model $iter $T $depth $p 1.0 $states >${outdir}/${states}s_${depth}d_${model}.out" >>$outdir/params.txt
-                time ./bin/factored_model 1DMaze $model $iter $T $depth $p 1.0 $states  >${outdir}/${states}s_${depth}d_${model}.out
+		echo "./bin/factored_model 1DMaze $model $iter $T $depth $p 1.0 $states >${outdir}/${states}s_${depth}d_${model}.out" >>$out_dir/params.txt
+		qsub -v"environment=$environment","iter=$iter","T=$T","depth=$depth","p=$p","pa=$pa","states=$states","out_dir=$out_dir" $script_dir/factored_model_sub.sh
             done
         done
     done
