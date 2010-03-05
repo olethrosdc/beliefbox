79f9ac82f4e1a4930c61ae75cf0e1c74f45cf6d3
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-03-05 22:28:42 +0000

    boing

--- trunk/src/statistics/tests/scripts/factored_model_gridworld.sh
+++ trunk/src/statistics/tests/scripts/factored_model_gridworld.sh
@@ -5,20 +5,20 @@ T=1000000
 iter=10
 states=0
 maze_dir=$HOME/projects/beliefbox/dat
-maze_name=maze8
+maze_name=maze3c
 for action_rand in 0.1 #1.0 0.5 0.1
 do
-    for maze_rand in 0.01 #0.01 0.1
+    for maze_rand in 0 0.01 #0.01 0.1
     do
-        for n_obs in 16
+        for n_obs in 2 16
         do
             outdir=~/experiments/bvmdp/gridworld/${maze_name}/${n_obs}obs/p${maze_rand}_pa${action_rand}
             mkdir -p $outdir
             echo -e "- action randomness:$action_rand\n- maze randomness: $maze_rand\n- observations: $n_obs\n- maze: $maze_name\n- iterations: $iter\n- T: $T" >${outdir}/run.params
             
-            for depth in 0 1 #2 3 4 5 6 7 8
+            for depth in 0 1 2 4 8 16 32 64 #2 3 4 5 6 7 8
             do
-                for model in BVMM BFMC FMC #BFMC # BVMM 
+                for model in BVMM2 #BVMM BFMC FMC #BFMC # BVMM 
                 do
                     echo $model $depth
                     echo "./bin/factored_model Gridworld $model $iter $T $depth $maze_rand $action_rand $maze_dir/$maze_name $n_obs >${outdir}/${states}s_${depth}d_${model}.out"
