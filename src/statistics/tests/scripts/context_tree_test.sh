5ce622df7461a0e74d043621dfed1faa0b58f031
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-04-17 13:09:53 +0000

    minor test diff

--- src/statistics/tests/scripts/context_tree_test.sh
+++ src/statistics/tests/scripts/context_tree_test.sh
@@ -6,14 +6,14 @@ do
     datafile=~/data/${dataset}.dat
     outdir=~/experiments/bvmm/${dataset}
     mkdir -p $outdir
-    for method in  PPM BMC BVMM #CTW PPM;
+    for method in  BVMM #PPM BMC BVMM #CTW PPM;
     do
         echo "Method: $method Dataset $datafile"
-        logfile=${outdir}/${method}.out;
+        logfile=${outdir}/${method}b.out;
         rm -f $logfile
         for depth in 1 2 3 4 5 6 7 8 9 10 11 12  14 16 20 24 28 32;
         do
-            ./bin/context_tree_test $method $depth ${outdir}/${method}${depth}.out $datafile >>$logfile
+            ./bin/context_tree_test $method $depth ${outdir}/${method}b${depth}.out $datafile >>$logfile
             tail -n 1 $logfile
         done;
         grep loss $logfile | cut -f 3 -d ":" | cut -f 1 -d "," >${outdir}/${method}.loss
