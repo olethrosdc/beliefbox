922ae2b52f545440e39943b9dbfaa7510e042891
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-04-21 22:47:24 +0000

    added factored tree

---
+++ trunk/src/models/tests/scripts/factored_tree_gridworld.sh
@@ -0,0 +1,17 @@
+model_name=BVMM
+n_iter=10
+T=10000000
+depth=1
+env_rand=0.01
+act_rand=0.1
+maze=~/projects/beliefbox/dat/maze1
+observations=16
+
+for depth in 1 2 4 8 16; 
+do
+    ./bin/factored_tree_rl Gridworld $model_name $n_iter $T $depth $env_rand $act_rand $maze $observations >out${depth}
+    awk -v K=1000 -v col=2 -f ~/scripts/moving_average.awk out${depth} >out${depth}ma
+done
+
+
+##gdb --args ./bin/factored_tree_rl Gridworld $model_name $n_iter $T $depth $env_rand $act_rand $maze $observations
