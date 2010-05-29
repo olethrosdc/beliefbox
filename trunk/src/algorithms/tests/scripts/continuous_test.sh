1b39dbf64a14a28937fb61717baeb8431200322e
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-05-29 20:47:23 +0000

    NIPS-1 script

--- trunk/src/algorithms/tests/scripts/continuous_test.sh
+++ trunk/src/algorithms/tests/scripts/continuous_test.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-n_runs=10
-n_episodes=1000
-n_steps=1000
+n_runs=100
+n_episodes=10000
+n_steps=10000
 gamma=0.99
 lambda=0.8
 n_actions=2
-for environment in MountainCar #MountainCar Pendulum
+for environment in MountainCar Pendulum #MountainCar Pendulum
 do
     for random in 0 0.01
     do
@@ -16,15 +16,16 @@ do
             resdir=$HOME/results/$environment/$algorithm/rand${random}
             mkdir -p $resdir
             echo ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment | tee $resdir/run.params
-            nice -n 19 ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment | grep REWARD > ${resdir}/K${K}reward.out 
+            nice -n 19 ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment | grep REWARD > ${resdir}/K${K}reward.out &
             algorithm=BVMM
             resdir=$HOME/results/$environment/$algorithm/rand${random}
             mkdir -p $resdir
-            for D in 1 2 3 4
+            for D in 0 1 2 3 4
             do
                 echo ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment $D | tee $resdir/run.params
-                nice -n 19 ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment $D | grep REWARD > ${resdir}/K${K}_D${D}_reward.out 
+                nice -n 19 ./bin/pomdp_algorithms $K $n_actions $gamma $lambda $random $n_runs $n_episodes $n_steps $algorithm $environment $D | grep REWARD > ${resdir}/K${K}_D${D}_reward.out &
             done
+            wait;
         done
     done
 done
