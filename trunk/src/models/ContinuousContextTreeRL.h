28374d5b4da3a10ade72b69a83403d7d359ab055
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-09-14 07:44:26 +0000

    minor mod

--- trunk/src/models/ContinuousContextTreeRL.h
+++ trunk/src/models/ContinuousContextTreeRL.h
@@ -37,67 +37,10 @@
 
     3. There is a full density estimation.
     
-    @see ContextTreeRL
+    @see ContextTreeRL, ConditionalKDContextTree
 */
 class ContinuousContextTreeRL
 {
-public:
-    // public classes
-    struct Node
-    {
-        int n_branches; ///< number of conditional observations
-        int n_outcomes; ///< number of observations to predict
-        int depth; ///< depth
-        Node* prev; ///< previous node
-        std::vector<Node*> next; ///< pointers to next nodes
-        Vector P; ///< probability of next symbols
-        Vector alpha; ///< parameters of next symbols
-        const real prior_alpha; ///< implicit prior value of alpha
-        real w; ///< backoff weight
-        real log_w; ///< log of w
-        real log_w_prior; ///< initial value
-        BetaDistribution reward_prior;
-        Node(int n_branches_,
-             int n_outcomes_);
-        Node(Node* prev_);
-        ~Node();
-        real Q; ///< last Q value of the context
-        real w_prod; ///< \f$\prod_k (1 - w_k)\f$
-        real context_probability; ///< last probability of the context
-        real Observe(Vector& x,
-					 int a,
-					 Vector& y,
-                     real r,
-                     real probability,
-                     std::list<Node*>& active_contexts);
-        real QValue(Vector& x,
-					int a,
-                    real Q_prev);
-        void Show();
-        int NChildren();
-    };  
-    // public methods
-    ContinuousContextTreeRL(int n_branches_, 
-                  int n_observations,
-                  int n_actions,
-                  int n_symbols_,
-                  int max_depth_= 0);
-    ~ContinuousContextTreeRL();
-    real Observe(Vector& x, int a, Vector& y, real r);
-    void Show();
-    int NChildren();
-    real QValue(int x);
-    real QLearning(real step_size,  real gamma, int observation, real reward);
-    real Sarsa(real step_size,  real gamma, int observation, real reward);
-protected: 
-    int n_branches;
-    int n_observations;
-    int n_actions;
-    int n_symbols;
-    int max_depth;
-    Node* root;
-    Ring<int> history;
-    std::list<Node*> active_contexts;
 };
 
 
