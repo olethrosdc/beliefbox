2d6d62e14877835b38d7f1c25484453617d38a79
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-10-04 15:50:49 +0000

    Added MultiMDP Value Iteration. Minor changes in CSCTRL

---
+++ trunk/src/algorithms/MultiMDPValueIteration.h
@@ -0,0 +1,67 @@
+// -*- Mode: c++ -*-
+// copyright (c) 2010 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+// $Revision$
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MULTI_MDP_VALUE_ITERATION_H
+#define MULTI_MDP_VALUE_ITERATION_H
+
+#include "DiscreteMDP.h"
+#include "DiscretePolicy.h"
+#include "real.h"
+#include <vector>
+#include "Matrix.h"
+
+class MultiMDPValueIteration
+{
+public:
+    Vector w;
+    const std::vector<const DiscreteMDP*> mdp_list;
+    real gamma;
+    int n_states;
+    int n_actions;
+    Vector V; ///< actual value
+    Vector dV;///< actual delta value
+    Vector pV; ///< previous value
+    Matrix Q; ///< actual q-value
+    Matrix dQ; ///< delta q-value
+    Matrix pQ; ///< previous q-value
+    int n_mdps; ///< The number of MDPs
+    std::vector<Vector> V_mu; ///< the MDPs individual value functions
+    real Delta;
+    MultiMDPValueIteration(const Vector& w,
+                           const std::vector<const DiscreteMDP*>& mdp_list_,
+                           real gamma_);
+    ~MultiMDPValueIteration();
+    void Reset();
+
+    void ComputeStateValues(real threshold, int max_iter=-1);
+    void ComputeStateActionValues(real threshold, int max_iter=-1);
+    inline real getValue (int state, int action)
+    {
+        assert(state>=0 && state < n_states);
+        assert(action>=0 && action < n_actions);
+        return Q(state,action);
+    }
+    inline real getValue (int state)
+    {
+        assert(state>=0 && state < n_states);
+        return V(state);
+    }
+    FixedDiscretePolicy* getPolicy();
+protected:
+    real ComputeActionValueForMDPs(int s, int a);
+    real ComputeStateActionValueForSingleMDP(int mu, int s, int a);
+
+};
+
+#endif
+
+
--- trunk/src/models/tests/scripts/test_continuous_tree.sh
+++ trunk/src/models/tests/scripts/test_continuous_tree.sh
@@ -5,19 +5,21 @@ exe=./bin/continuous_factored_tree_rl
 environment=Pendulum
 model=BVMM
 T=1000000
-iter=100
+iter=1
 erand=0.01
 arand=0.01
 ulimit -v 2000000
+resdir=resdir/half_weights_low_threshold
+mkdir -p $resdir
 
 for D in 1 2 3 4 5 6 7 8
 do
-    for environment in MountainCar MountainCar3D # Pendulum MountainCar #MountainCar3D
+    for environment in MountainCar MountainCar3D Pendulum LinearBandit
     do
-	echo $T $D $D_c
-    cmdline="$exe $environment $model $iter $T $D $erand $arand"
-	echo $cmdline
-	nice -n 19 $cmdline | grep STATS >${environment}_d${D}.out &
+	    echo $T $D $D_c
+        cmdline="$exe $environment $model $iter $T $D $erand $arand"
+	    echo $cmdline >$resdir/${environment}_d${D}.cmd
+	    nice -n 9 $cmdline | grep STATS >$resdir/${environment}_d${D}.out &
     done
     wait;
 done
