e7ee1c8b626ea3c5149fcaeffa8d06b35809c2b5
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-10-20 15:38:46 +0000

    uga

--- trunk/src/models/tests/scripts/classify.sh
+++ trunk/src/models/tests/scripts/classify.sh
@@ -1,7 +1,7 @@
 datadir=~/data/classification
 resdir=~/results/classification
 mkdir -p resdir
-for dataset in optdigits pendigits
+for dataset in optdigits pendigits spect
 do
     train=$datadir/$dataset/train.dat
     test=$datadir/$dataset/test.dat
@@ -11,20 +11,21 @@ do
     echo "Test set: $test"
     outdir=$resdir/$dataset/hash_mix
     mkdir -p $outdir
-	# for knn in 1 2 3 4
-	# do
-	# 	fname=$outdir/knn${knn}
-	# 	/usr/bin/time --verbose --output $fname.time ./bin/classify --train $train --test $test --randomise --normalise --knn $knn  >$fname.out
-	# 	grep TRAIN $fname.out >${fname}_train.res
-	# 	grep TEST $fname.out >${fname}_test.res
-	# 	for mix in 1 2 4 8 16
-	# 	do
-	# 		fname=$outdir/knn${knn}_mix${mix}
-	# 		/usr/bin/time --verbose --output $fname.time ./bin/classify --train $train --test $test --randomise --normalise --knn $knn --mixture $mix >$fname.out
-	# 		grep TRAIN $fname.out >${fname}_train.res
-	# 		grep TEST $fname.out >${fname}_test.res
-	# 	done
-	# done
+	for knn in 1 2 3 4
+	do
+		fname=$outdir/knn${knn}
+		/usr/bin/time --verbose --output $fname.time ./bin/classify --train $train --test $test --randomise --normalise --knn $knn  >$fname.out
+		grep TRAIN $fname.out >${fname}_train.res
+		grep TEST $fname.out >${fname}_test.res
+		for mix in 1 2 4 8 16
+		do
+			fname=$outdir/knn${knn}_mix${mix}
+			/usr/bin/time --verbose --output $fname.time ./bin/classify --train $train --test $test --randomise --normalise --knn $knn --mixture $mix >$fname.out
+			grep TRAIN $fname.out >${fname}_train.res
+			grep TEST $fname.out >${fname}_test.res
+		done
+	done
+
 	fname=$outdir/tree${knn}
 	for tree in 1 2 4 8 16 32 64 128
 	do
