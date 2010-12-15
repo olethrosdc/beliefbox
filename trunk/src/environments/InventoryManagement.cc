cc56c7837c6c2f9dd6fc920e21a6ffa26e2c6347
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-12-15 15:56:25 +0000

    inventory management fixes and test

--- trunk/src/environments/InventoryManagement.cc
+++ trunk/src/environments/InventoryManagement.cc
@@ -19,21 +19,19 @@
 
 InventoryManagement::InventoryManagement(int period_,
                                          int max_items_,
-                                         real demand_)
-    : period(period_), max_items(max_items_), demand(demand_)
+                                         real demand_,
+                                         real margin_)
+    : period(period_), max_items(max_items_), demand(demand_), margin(margin_)
 {
     int n_actions = max_items + 1;
     int n_states = max_items + 1;
+    
+    assert (max_items > 1);
+    assert (margin >= 1);
+    assert (demand >= 0 && demand <= 1);
+    assert (period > 0);
 
     mdp = new DiscreteMDP(n_states, n_actions, NULL, NULL);
-
-    
-    dist.resize(period + 1);
-    for (int i=0; i<=period; i++) {
-        dist[i] = new BinomialDistribution(period, i, 1);
-        //printf("%d: %f\n", i, dist[i]->getMean());
-    }
-
 
     for (int s=0; s<n_states; s++) {
         for (int a=0; a<n_actions; a++) {
@@ -41,22 +39,27 @@ InventoryManagement::InventoryManagement(int period_,
             if (s+a > max_items) {
                 order = max_items - s;
             }
+            real expected_reward = - order; // add the order costs first
             for (int s2=0; s2<n_states; s2++) {
                 int d = s + order - s2;
                 if (d < 0) {
                     d = 0;
                 }
-                mdp->setTransitionProbability(s, a, s2, binomial(period, d)*pow(demand, d)*pow(1-demand, period - d));
-                mdp->setRewardDistribution(s, a, dist[d]);
+                real P =  binomial(period, d)*pow(demand, d)*pow(1-demand, period - d);
+                expected_reward += margin * P*d;
+                mdp->setTransitionProbability(s, a, s2, P);
+               
             }
+
+            dist.push_back(new SingularDistribution(expected_reward));
+            mdp->setRewardDistribution(s, a, dist.back());
         }
     }
 }
 
 InventoryManagement::~InventoryManagement()
 {
-    for (int i=0; i<=period; i++) {
+    for (int i=0; i<dist.size(); i++) {
         delete dist[i];
     }
-
 }
--- trunk/src/environments/InventoryManagement.h
+++ trunk/src/environments/InventoryManagement.h
@@ -14,20 +14,22 @@
 #define INVENTORY_MANAGEMENT_H
 
 #include "DiscreteMDP.h"
-#include "BinomialDistribution.h"
+#include "Distribution.h"
 #include <string>
 #include <vector>
 
-// The inventory management task.
+/// The inventory management task.
 class InventoryManagement {
 protected:
-    int period;
-    int max_items;
-    real demand;
-    std::vector<BinomialDistribution*> dist;
-    DiscreteMDP* mdp;
+    int period; ///< This is the period (number of steps) between orders
+    int max_items; ///< This is the maximum number of items that can be stocked
+    real demand; ///< This is the probability that an item will be bought at each step
+    real margin; ///< this is the ratio (greater than 1) of the retail value of the item versus the order cost
+    std::vector<SingularDistribution*> dist; ///< container for the reward distributions
+    DiscreteMDP* mdp; ///< points to the MDP
+
 public:
-    InventoryManagement(int period_, int max_items_, real demand_);
+    InventoryManagement(int period_, int max_items_, real demand_, real margin_);
     ~InventoryManagement();
     DiscreteMDP* getMDP() {return mdp;}
 };
