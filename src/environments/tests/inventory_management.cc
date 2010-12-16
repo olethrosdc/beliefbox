a50ae3f3fff6781322135e3be7b022e8e3a2ed74
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-12-16 09:17:25 +0000

    Fixed inventory management

--- src/environments/tests/inventory_management.cc
+++ src/environments/tests/inventory_management.cc
@@ -1,17 +1,21 @@
 #include "InventoryManagement.h"
 #include "ValueIteration.h"
 #include "Matrix.h"
+#include "Gridworld.h"
 
 int main(void)
 {
     real gamma = 0.5;
     InventoryManagement inventory_management(5, 5, 0.1, 1.1);
+    Gridworld gridworld("/home/olethros/projects/beliefbox/dat/maze1", 0.01);
     const DiscreteMDP* mdp = inventory_management.getMDP();
+    //const DiscreteMDP* mdp = gridworld.getMDP();
+
     
     mdp->ShowModel();
     ValueIteration value_iteration(mdp, gamma);
-    value_iteration.ComputeStateActionValues(10e-6,0);
-    value_iteration.ComputeStateValues(10e-6,0);
+    value_iteration.ComputeStateActionValues(10e-6,1000);
+    value_iteration.ComputeStateValues(10e-6,1000);
 
     Matrix Q(mdp->GetNStates(), mdp->GetNActions());
     for (int s=0; s<mdp->GetNStates(); ++s) {
