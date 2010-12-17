e9221f784b12eb8aed90b3ccabe5aedf62bc453a
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2010-12-17 00:09:49 +0000

    minor fixes

--- src/algorithms/tests/scripts/test_gridworld.sh
+++ src/algorithms/tests/scripts/test_gridworld.sh
@@ -1,12 +1,12 @@
 maze=$HOME/projects/beliefbox/dat/maze1
 width=8
 height=8
-T=1000
-n_runs=1
-n_episodes=1000
+T=10000
+n_runs=10
+n_episodes=100
 rand=0.01
 lambda=0.9
-gamma=0.99
+gamma=0.9
 model=QLearning
 S=8
 A=3
@@ -14,22 +14,23 @@ Environment=Gridworld
 
 model=QLearning
 echo $model
-time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --maze_height=$height --maze_width=$width --algorithm $model --environment $Environment > $model.out
+time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --algorithm $model --environment $Environment > $model.out
 
 model=Sarsa
 echo $model
-time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --maze_height=$height --maze_width=$width --algorithm $model --environment $Environment > $model.out
+time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --algorithm $model --environment $Environment > $model.out
 
 
 
 model=Model
 echo $model
-time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --maze_height=$height --maze_width=$width --algorithm $model --environment $Environment > $model.out
+time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --algorithm $model --environment $Environment > $model.out
+
 
 
 model=Sampling
 echo $model
-time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --maze_height=$height --maze_width=$width --algorithm $model --environment $Environment > $model.out
+time ./bin/online_algorithms --gamma $gamma --lambda $lambda --n_runs $n_runs --n_episodes $n_episodes --n_steps $T --maze_name=$maze --algorithm $model --max_samples 4 --environment $Environment  > $model.out
 
 wait;
 
