5b83400d755078b9e9f4ae97901345993c3d2954
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-01-19 00:10:22 +0000

    fixed some things, but compilation is broken

--- trunk/src/statistics/BoundedLogNormalDistribution.h
+++ trunk/src/statistics/BoundedLogNormalDistribution.h
@@ -15,7 +15,6 @@
 #include "NormalDistribution.h"
 #include "Matrix.h"
 #include "Vector.h"
-
 #include "Student.h"
 
 
@@ -25,28 +24,20 @@
     \f[
     g_i(y) = (c_i - y_i)
     \ln \left[
-    4(d_i)^{-2} (y_i - a_i)(b_i - x_i)
+    (d_i)^{-2} (y_i - a_i)(b_i - x_i)
     \right]
     \f] 
     where \f$c = \frac{a+b}{2}\f$ and \f$d = \frac{b-a}{2}\f$.
     Then \f$x_t = g(y_t)\f$, are normally distributed with unknown
-    mean \f$m\f$ and precision matrix \f$r\f$. We denote the normal
-    density by \f$f(x \mid m, r)\f$. The prior joint parameter
-    distribution \f$\xi_0(m, r) = \xi_0(m \mid r) \xi_0(r)\f$ is
-    specified as follows.  The conditional distribution of \f$m\f$
-    given \f$r\f$ is \f$\xi_0(m \mid r) = f(m \mid \mu_0, \tau_0 r)\f$
-    and the marginal of the precision is \f$\xi_0(r) = g(r \mid
-    \alpha_0, T_0)\f$.
-
-    The predictive posterior distribution \f$\xi_n(x_{n+1})\f$ is
-    actually a generalised student-t distribution, but here we are
-    hacking it as a normal.
+    mean \f$m\f$ and precision matrix \f$r\f$. 
  */
 class BoundedLogNormal
 {
 protected:
     MultivariateNormalUnknownMeanPrecision* normal_density;
     int n_dim;
+    Vector transform(const Vector& x) const;
+    Vector inverse_transform(const Vector& x) const;
 public:
     Vector a; ///< lower bound
     Vector b; ///< upper bound
