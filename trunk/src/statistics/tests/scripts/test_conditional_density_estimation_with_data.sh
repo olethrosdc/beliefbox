c0cfcd6ea80bdeed0f1333825aa9247b2b8ec441
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-02-10 10:21:55 +0000

    minor diff

--- trunk/src/statistics/tests/scripts/test_conditional_density_estimation_with_data.sh
+++ trunk/src/statistics/tests/scripts/test_conditional_density_estimation_with_data.sh
@@ -1,33 +1,17 @@
-ulimit -v 2000000
-for i in 1 2 4 8 16 32 64 128 
-do 
-    echo $i $j
-    time ./bin/conditional_density_estimation --joint --max_depth $i --data ~/data/regression/maxtemp_one.dat >temp_tde_${i}.out; grep P_XY temp_tde_${i}.out >results/temp_tde_${i}.dat;
-done;
-
-for i in 1 2 4 8 12
-do 
-    for j in 1 2 4 8 12
-    do 
-        echo $i $j
-        time ./bin/conditional_density_estimation --n_inputs 1 --max_depth $i --max_depth_cond $j --data ~/data/regression/maxtemp_one.dat >temp_tcde_${i}_${j}.out; 
-        grep P_Y_X temp_tcde_${i}_${j}.out >results/temp_tcde_${i}_${j}.dat;
-    done;
-done
-
-
-
 
 data=~/data/distributions/ring_gauss.dat
-b=1
-for T in 10 100 1000 10000 100000 1000000
+b=10
+for T in 10 100 1000 10000
 do 
     i=8;
     j=8;
-    time ./bin/conditional_density_estimation --n_inputs 1 --max_depth $i --max_depth_cond $j --T $T --data $data --joint >ring_tde_T${T}_${i}_${j}.out; 
-    grep P_XY ring_tde_T${T}_${i}_${j}.out >results/ring_tde_T${T}_${i}_${j}.dat;
     time ./bin/conditional_density_estimation --n_inputs 1 --max_depth $i --max_depth_cond $j --T $T --data $data >ring_tcde_T${T}_${i}_${j}.out; 
     grep P_Y_X ring_tcde_T${T}_${i}_${j}.out >results/ring_tcde_T${T}_${i}_${j}.dat;
+    time ./bin/kernel_conditional_density_estimation --bandwidth $b --tune --n_inputs 1 --max_depth $i --max_depth_cond $j --T $T --data $data >ring_kcde_T${T}.out; 
+    grep P_Y_X ring_kcde_T${T}.out >results/ring_kcde_T${T}.dat;
+
+    time ./bin/double_kernel_cde --bandwidth $b --tune --n_inputs 1 --max_depth $i --max_depth_cond $j --T $T --data $data >ring_dkcde_T${T}.out; 
+    grep P_Y_X ring_dkcde_T${T}.out >results/ring_dkcde_T${T}.dat;
 done;
 
 
