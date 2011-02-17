f44957dd8f0a40bc37171fafa0fe0d4beb9ad141
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-02-17 22:17:50 +0000

    Added DiscreteChain
    Added vector test
    
    Fixed PolicyEvaluation iterations bug.
    
    Changed inverse_rl to use new features

---
+++ trunk/src/core/tests/vector.cc
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; -*- */
+// copyright (c) 2010 by Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef MAKE_MAIN
+
+#include "Vector.h"
+
+int main(int argc, char** argv)
+{
+    
+    Vector x(4);
+    for (uint i=0; i<x.Size(); ++i) {
+        x(i) = i;
+    }
+    x.print(stdout);
+    x *= 2;
+    x.print(stdout);
+    x = 3;
+    x.print(stdout);
+    return 0;
+}
+
+#endif
