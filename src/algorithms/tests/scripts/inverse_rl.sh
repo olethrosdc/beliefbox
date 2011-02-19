deb87e98ae5a50829cad13ec21eda5183ebb2d15
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-02-19 15:09:42 +0000

    Parallelised scripts

--- src/algorithms/tests/scripts/inverse_rl.sh
+++ src/algorithms/tests/scripts/inverse_rl.sh
@@ -7,38 +7,43 @@ do
     environment=Optimistic
     resdir=$baseres/$environment
     mkdir -p $resdir
-    echo $environment $n_states
-    ./bin/inverse_rl  --environment $environment --n_runs $n_runs --iterations $iter | grep DV >$resdir/${iter}i_DV.out
+    for gamma in 0.9 0.95 0.99 0.999
+    do
+        echo $environment $gamma
+        ./bin/inverse_rl  --environment $environment --gamma $gamma --n_runs $n_runs --iterations $iter | grep DV >$resdir/${iter}i_${gamma}g_DV.out &
+    done
+    wait;
 
     environment=Gridworld
     for maze in maze0 maze1 maze2 maze3
     do
         resdir=$baseres/$environment/$maze
         mkdir -p $resdir
-        for n_states in 2 4 8 16 32 64 128
-        do
-            echo $environment $maze $n_states
-            ./bin/inverse_rl  --environment $environment --maze_name $maze --n_runs $n_runs --iterations $iter | grep DV >$resdir/${iter}i_DV.out
-        done
+        echo $environment $maze
+        maze_path=$HOME/projects/beliefbox/dat/$maze
+        ./bin/inverse_rl  --environment $environment --maze_name $maze_path --n_runs $n_runs --iterations $iter | grep DV >$resdir/${iter}i_DV.out &
     done
+    wait;
 
     environment=Chain
     resdir=$baseres/$environment
     mkdir -p $resdir
-    for n_states in 2 4 8 16 #32 64 128
+    for n_states in 2 4 8 16 
     do
         echo $environment $n_states
-        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --iterations $iter | grep DV >$resdir/${n_states}s_${iter}i_DV.out
+        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --iterations $iter | grep DV >$resdir/${n_states}s_${iter}i_DV.out &
     done
+    wait;
 
     environment=RandomMDP
     resdir=$baseres/$environment
     mkdir -p $resdir
-    for n_states in 4 8 16 32 64 128 256
+    for n_states in 4 16 64 256
     do
         echo $environment $n_states
-        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --iterations $iter | grep DV >$resdir/${n_states}s_${iter}i_DV.out
+        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --iterations $iter | grep DV >$resdir/${n_states}s_${iter}i_DV.out &
     done
+    wait;
 
 done
 
