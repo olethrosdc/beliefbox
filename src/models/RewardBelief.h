c1fa488c757bf53ff89906084bb805b2ffd5baff
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-03-28 15:35:30 +0000

    Reward belief update

--- src/models/RewardBelief.h
+++ src/models/RewardBelief.h
@@ -12,38 +12,55 @@
 #ifndef REWARD_BELIEF_H
 #define REWARD_BELIEF_H
 
+#include "RewardDistribution.h"
+#include "Vector.h"
+#include <vector>
+
+
 /** A belief about rewards in discrete spaces.
     
-    We wish to split this belief into bits.
  */
 class DiscreteRewardBelief
 {
 protected:
-	int n_states; ///< number of states
-	int n_actions; ///< number of actions
-    std::vector<Distribution*> R; ///< reward distribution
-	std::vector<Distribution*> distribution_vector; ///< for malloc
-	Vector ER; ///< expected reward
-    inline int getID (int s, int a) const
-    {
-        
-        assert(s>=0 && s<n_states);
-        assert(a>=0 && a<n_actions);
-        return s*n_actions + a;
-    }
-
+    int n_states;
+    int n_actions;
 public:
     // only set up
     DiscreteRewardBelief(int n_states_, int n_actions_)
         : n_states(n_states_), n_actions(n_actions_)
     {}
         
-    virtual ~DiscreteRewardBelief();
-    virtual real generate(int state, int action) const;
-    virtual real expected(int state, int action) const;
-    virtual real pdf(int state, int action, real reward) const;
-	void setRewardDistribution(int s, int a, Distribution* reward);
-	void addRewardDistribution(int s, int a, Distribution* reward);
-	void addFixedReward(int s, int a, real reward);
-	void setFixedReward(int s, int a, real reward);
+    virtual ~DiscreteRewardBelief()
+    {}
+
+    virtual real Update(int state, int action, real reward) = 0;
 };
+
+
+/** A belief about a finite set of reward distributions in discrete
+    spaces.
+    
+ */
+class FiniteDiscreteRewardBelief
+{
+protected:
+    std::vector<const DiscreteSpaceRewardDistribution*>& rewards; ///< reward distributions
+    Vector probabilities; ///< vector of probabilites for each reward distribution
+public:
+    FiniteDiscreteRewardBelief(int n_states_, int n_actions_,
+                               std::vector<const DiscreteSpaceRewardDistribution*>& rewards_,
+                               Vector probabilities_)
+        : DiscreteRewardBelief(n_states_, n_actions_),
+          rewards(rewards_),
+          probabilities(probabilites_)
+    {
+        assert(probabilites.Size() == reward
+    }
+        
+    virtual ~FiniteDiscreteRewardBelief()
+    {}
+    virtual real Update(int state, int action, real reward);
+};
+
+
