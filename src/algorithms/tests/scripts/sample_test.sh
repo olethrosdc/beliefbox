d0c0e06e84fa31606761b2faf7a1d553f3f2ea6d
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-05-12 19:36:11 +0000

    sample tes

---
+++ src/algorithms/tests/scripts/sample_test.sh
@@ -0,0 +1,31 @@
+
+runs=10000
+steps=5000
+gamma=0.95
+epsilon=0.0
+
+## task=Chain
+## params="--n_states 5 --n_actions 2 --environment Chain --gamma $gamma --epsilon $epsilon --n_runs $runs --n_steps $steps"
+
+task=RiverSwim
+params="--environment RiverSwim --gamma $gamma --epsilon $epsilon --n_runs $runs --n_steps $steps"
+
+outdir=$HOME/results/${task}/epsilon${epsilon}
+mkdir -p $outdir
+for algorithm in QLearning Sarsa Model
+do
+    time ./bin/online_algorithms --algorithm $algorithm $params >$outdir/${algorithm}.out
+    grep PAYOFF $outdir/${algorithm}.out > $outdir/${algorithm}.payoff
+    grep RUN $outdir/${algorithm}.out > $outdir/${algorithm}.reward
+    rm $outdir/${algorithm}.out
+done
+
+for i in 1 2 3 4 5 6 7 8;
+do 
+    echo $i; 
+    time ./bin/online_algorithms --algorithm Sampling --max_samples=${i} $params >$outdir/sampling${i}.out;
+    grep PAYOFF $outdir/sampling${i}.out >$outdir/sampling${i}.payoff; 
+    grep RUN $outdir/sampling${i}.out >$outdir/sampling${i}.reward; 
+    rm $outdir/sampling${i}.out
+done
+
