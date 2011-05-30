2f64e2dfcf8fa5be51403c6cd280b3b045f8f8ed
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-05-30 21:03:34 +0000

    fixed script -This line, and those below, will be ignored--
    M    tests/scripts/inverse_rl.sh
    M    tests/scripts/inverse_rl_chain.sh

--- trunk/src/algorithms/tests/scripts/inverse_rl.sh
+++ trunk/src/algorithms/tests/scripts/inverse_rl.sh
@@ -4,15 +4,15 @@ n_runs=100
 for steps in 100 200 400 800 1600 3200 6400 12800 25600 51200 102400
 do
 
-    environment=Optimistic
-    resdir=$baseres/$environment
-    mkdir -p $resdir
-    for gamma in 0.9 0.95 0.99 0.999
-    do
-        echo $environment $gamma
-        ./bin/inverse_rl  --environment $environment --gamma $gamma --n_runs $n_runs --n_steps $steps | tee $resdir/${steps}T_${gamma}g.out | grep DV >$resdir/${steps}T_${gamma}g_DV.out &
-    done
-    wait;
+#    environment=Optimistic
+#    resdir=$baseres/$environment
+#    mkdir -p $resdir
+#    for gamma in 0.9 0.95 0.99 0.999
+#    do
+#        echo $environment $gamma
+#        ./bin/inverse_rl  --environment $environment --gamma $gamma --n_runs $n_runs --n_steps $steps | tee $resdir/${steps}T_${gamma}g.out | grep DV >$resdir/${steps}T_${gamma}g_DV.out &
+#    done
+#    wait;
 
     environment=Gridworld
     for maze in maze0 maze1 maze2 maze3
@@ -25,35 +25,35 @@ do
     done
     wait;
 
-    environment=Chain
-    resdir=$baseres/$environment
-    mkdir -p $resdir
-    for n_states in 2 4 8 16 
-    do
-        echo $environment $n_states
-        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee $resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
-    done
-    wait;
+ #   environment=Chain
+ #   resdir=$baseres/$environment
+ #   mkdir -p $resdir
+ #   for n_states in 2 4 8 16 
+ #   do
+ #       echo $environment $n_states
+ #       ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee $resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
+  #  done
+  #  wait;
 
-    environment=RiverSwim
-    resdir=$baseres/$environment
-    mkdir -p $resdir
-    for n_states in 3 4 5 6
-    do
-        echo $environment $n_states
-        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee $resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
-    done
-    wait;
+#    environment=RiverSwim
+#    resdir=$baseres/$environment
+#    mkdir -p $resdir
+#    for n_states in 3 4 5 6
+#    do
+#        echo $environment $n_states
+#        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee $resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
+#B    done
+#    wait;
 
-    environment=RandomMDP
-    resdir=$baseres/$environment
-    mkdir -p $resdir
-    for n_states in 4 16 64 256
-    do
-        echo $environment $n_states
-        ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee >$resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
-    done
-    wait;
+ #   environment=RandomMDP
+ #   resdir=$baseres/$environment
+ #   mkdir -p $resdir
+ #   for n_states in 4 16 64 256
+ #   do
+ #       echo $environment $n_states
+ #       ./bin/inverse_rl --n_states $n_states --environment $environment --n_runs $n_runs --n_steps $steps | tee >$resdir/${n_states}s_${steps}T.out | grep DV >$resdir/${n_states}s_${steps}T_DV.out &
+ #   done
+ #   wait;
 
 done
 
