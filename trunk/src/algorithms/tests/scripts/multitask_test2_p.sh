64d98abcae62ab90e1d065e8232116ce770baad1
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-08-22 08:07:53 +0000

    added another script

---
+++ trunk/src/algorithms/tests/scripts/multitask_test2_p.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+samples=10000
+gamma=0.95
+n_steps=100
+accuracy=0.01
+n_runs=10
+task=Gridworld
+randomness=1.0
+epsilon=0.1
+maze=$HOME/projects/beliefbox/dat/maze2
+for n_demonstrations in 20
+do
+    for n_tasks in 1 2 5 10
+    do 
+        echo $n_tasks
+        time ./bin/multitask_inverse_rl --MonteCarlo --n_chain_samples $samples --n_tasks $n_tasks --n_demonstrations $n_demonstrations --gamma $gamma --environment $task --randomness $randomness --n_steps $n_steps --accuracy $accuracy --n_runs $n_runs --epsilon $epsilon --maze_name $maze | tee maze_out_d${n_demonstrations}_t${n_tasks} &
+    done
+    wait;
+done
