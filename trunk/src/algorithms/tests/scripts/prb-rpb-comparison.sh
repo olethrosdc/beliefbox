f0d8a89f46a1d3d2d6ed88c6cba3206168b95396
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2011-09-26 15:33:02 +0000

    PRB/RPB mix

--- trunk/src/algorithms/tests/scripts/prb-rpb-comparison.sh
+++ trunk/src/algorithms/tests/scripts/prb-rpb-comparison.sh
@@ -1,9 +1,10 @@
 outdir=$HOME/results/inverse_rl/Chain/
-for samples in 1 10 20 50 100 200 500 1000 2000 5000 10000
+runs=100
+for samples in 2000 5000 10000
 do
     for n_steps in 10 100 1000 10000
     do
-        inverse_rl --algorithm Model --n_steps ${n_steps} --environment Chain --n_states 5 --n_chains 1 --iterations $samples --n_chain_samples $samples --MonteCarlo --n_runs 1000 | tee $outdir/out_${n_steps}steps_${samples}samples &
+        inverse_rl --algorithm Model --n_steps ${n_steps} --environment Chain --n_states 5 --n_chains 1 --iterations $samples --n_chain_samples $samples --MonteCarlo --n_runs $runs | tee $outdir/out_${n_steps}steps_${samples}samples &
     done
     wait;
 done
