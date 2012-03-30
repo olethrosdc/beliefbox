9d96255b2e9467b1c56a920b287db6e3a14b9b4b
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2012-03-30 22:47:20 +0000

    uga

---
+++ trunk/src/algorithms/tests/scripts/pareto_ucb.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+exc=$HOME/projects/beliefbox/src/algorithms/tests/bin/pareto_ucb
+actions=16
+outcomes=4
+horizon=100000
+runs=1000
+
+for actions in 2 4 8 16 32 64 128 256
+do
+	resdir=$HOME/results/pareto_ucb/${actions}A_${outcomes}S_${horizon}T_WUCB/
+	mkdir -p $resdir
+	results=$resdir/regret.out
+	errors=$resdir/errors.out
+	echo $actions $outcomes $horizon $runs $epsilon
+	time ${exc} $actions $outcomes $horizon $runs 2 0.0 > $results 2> $errors
+	
+	for epsilon in 0.0 0.01 0.1 0.5 1.0
+	do
+		resdir=$HOME/results/pareto_ucb/${actions}A_${outcomes}S_${horizon}T_${epsilon}E/
+		mkdir -p $resdir
+		results=$resdir/regret.out
+		errors=$resdir/errors.out
+		echo $actions $outcomes $horizon $runs $epsilon
+		time ${exc} $actions $outcomes $horizon $runs 1 $epsilon > $results 2> $errors
+	done
+done
