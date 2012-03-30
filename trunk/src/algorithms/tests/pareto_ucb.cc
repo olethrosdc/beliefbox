aac666d6d5c25501578d98a657a25aed7c034539
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2012-03-30 14:49:36 +0000

    pareto ucb

--- trunk/src/algorithms/tests/pareto_ucb.cc
+++ trunk/src/algorithms/tests/pareto_ucb.cc
@@ -14,37 +14,47 @@
 #include "Matrix.h"
 #include "Dirichlet.h"
 #include "Sarsa.h"
-
+#include "MersenneTwister.h"
 
 class ObjectiveBanditPolicy
 {
 public:
 	const int n_actions;
 	const int n_outcomes;
-	ObjectiveBanditPolicy(int n_actions_, int n_outcomes_)
-		: n_actions(n_actions_), n_outcomes(n_outcomes_)
+	RandomNumberGenerator& rng;
+	ObjectiveBanditPolicy(int n_actions_, 
+						  int n_outcomes_, 
+						  RandomNumberGenerator& rng_)
+		: n_actions(n_actions_), n_outcomes(n_outcomes_), rng(rng_)
 	{
 	}
 	virtual ~ObjectiveBanditPolicy()
 	{}
-	virtual int getAction(const Vector& payoff) const = 0;
+	/// get the greedy action
+	virtual int getGreedyAction(const Vector& payoff) const = 0;
+	/// act
 	virtual int Act(const Vector& payoff, int outcome) = 0;
 };
 
 
 
-class GreedyObjectiveBandit : public ObjectiveBanditPolicy
+class EpsilonGreedyObjectiveBandit : public ObjectiveBanditPolicy
 {
 protected:
 	int action;
 public:
 	Matrix N; ///< probability matrix
 	Matrix P; ///< probability matrix
-	GreedyObjectiveBandit(int n_actions_, int n_outcomes_)
-		: ObjectiveBanditPolicy(n_actions_, n_outcomes_),
+	real epsilon; ///< randomness
+	EpsilonGreedyObjectiveBandit(int n_actions_,
+								 int n_outcomes_,
+								 RandomNumberGenerator& rng_,
+								 real epsilon_)
+		: ObjectiveBanditPolicy(n_actions_, n_outcomes_, rng_),
 		  action(-1),
 		  N(n_actions, n_outcomes),
-		  P(n_actions, n_outcomes)
+		  P(n_actions, n_outcomes),
+		  epsilon(epsilon_)
 	{
 		real p = 1.0 / (real) n_outcomes;
 		for (int i=0; i<n_actions; ++i) {
@@ -53,25 +63,85 @@ public:
 				P(i,j) = p;
 			}
 		}
+		assert(epsilon >= 0.0 && epsilon <= 1.0);
 	}
 
-	virtual ~GreedyObjectiveBandit()
+	virtual ~EpsilonGreedyObjectiveBandit()
 	{
 	}
 
-	virtual int getAction(const Vector& payoff) const
+	/// get the greedy action
+	virtual int getGreedyAction(const Vector& payoff) const
 	{
 		const Matrix& rP = P;
 		return ArgMax(rP * payoff);
 	}
+
+	/// act
 	virtual int Act(const Vector& payoff, int outcome)
 	{
+		if (rng.uniform() < epsilon) {
+			action = rng.discrete_uniform(n_actions);
+		}
+
 		if (action >= 0 && outcome >= 0) {
 			N(action, outcome)++;
 			Vector p = N.getRow(action);
 			P.setRow(action, p / p.Sum());
 		}
-		action = getAction(payoff);
+		action = getGreedyAction(payoff);
+
+		return action;
+	}
+};
+
+
+class WeissmanObjectiveBandit : public ObjectiveBanditPolicy
+{
+protected:
+	int action;
+public:
+	Matrix N; ///< incidence matrix
+	Matrix P; ///< probability matrix
+	real delta; ///< randomness
+	int T; ///< number of time-steps
+	std::vector<int> plays; ///< number of plays
+	WeissmanObjectiveBandit(int n_actions_,
+							int n_outcomes_,
+							RandomNumberGenerator& rng_)
+		: ObjectiveBanditPolicy(n_actions_, n_outcomes_, rng_),
+		  action(-1),
+		  N(n_actions, n_outcomes),
+		  P(n_actions, n_outcomes),
+		  delta(1.0),
+		  T(0),
+		  plays(n_actions)
+	{
+		real p = 1.0 / (real) n_outcomes;
+		for (int i=0; i<n_actions; ++i) {
+			plays[i] = 0;
+			for (int j=0; j<n_outcomes; ++j) {
+				N(i,j) = 0.5;
+				P(i,j) = p;
+			}
+		}
+	}
+
+	virtual ~WeissmanObjectiveBandit()
+	{
+	}
+
+	/// get the greedy action
+	virtual int getGreedyAction(const Vector& payoff) const
+	{
+		const Matrix& rP = P;
+		return ArgMax(rP * payoff);
+	}
+
+	/// act
+	virtual int Act(const Vector& payoff, int outcome)
+	{
+		
 
 		return action;
 	}
@@ -83,18 +153,18 @@ struct RegretPair
 	real worst;
 };
 
-RegretPair getRegret(const Matrix& P, const GreedyObjectiveBandit& policy)
+RegretPair getRegret(const Matrix& P, const EpsilonGreedyObjectiveBandit& policy)
 {
 	RegretPair regret;
 	regret.average = 0.0;
 	regret.worst = 0.0;
-	int n_actions = policy.n_actions;
+	//int n_actions = policy.n_actions;
 	int n_outcomes = policy.n_outcomes;
 
 	for (int i=0; i<n_outcomes; ++i) {
 		Vector payoff(n_outcomes);
 		payoff(i) = 1.0;
-		int action = policy.getAction(payoff);
+		int action = policy.getGreedyAction(payoff);
 		const Vector& r_payoff = payoff;
 		Vector U = P * r_payoff;
 		real delta = Max(U) - U(action);
@@ -112,23 +182,30 @@ int main (int argc, char** argv)
 {
 
     srand48(1228517343);
+	MersenneTwisterRNG rng;
+	rng.manualSeed(1228517343);
     //RandomNumberFile rng("./dat/r1e7.bin");
 	int n_actions = 4;
 	int n_outcomes = 4;
 	int horizon = 1000;
 	int n_experiments = 10;
+	real epsilon = 0.0;
 
-	if (argc <= 4) {
-		fprintf(stderr, "arguments: n_actions n_outcomes horizon n_runs\n");
+	if (argc <= 5) {
+		fprintf(stderr, "arguments: n_actions n_outcomes horizon n_runs epsilon\n");
 		exit(-1);
 	}
 	n_actions = atoi(argv[1]);
 	n_outcomes = atoi(argv[2]);
 	horizon = atoi(argv[3]);
 	n_experiments = atoi(argv[4]);
+	epsilon = atof(argv[5]);
 
 	Vector worst_case(horizon);
 	Vector average_case(horizon);
+
+
+
     for (int experiment=0; experiment<n_experiments; experiment++) {
 
 		Matrix P_sa(n_actions, n_outcomes);
@@ -136,7 +213,7 @@ int main (int argc, char** argv)
 		for (int i=0; i<n_actions; ++i) {
 			P_sa.setRow(i, prior.generate());
 		}
-		GreedyObjectiveBandit policy(n_actions, n_outcomes);
+		EpsilonGreedyObjectiveBandit policy(n_actions, n_outcomes, rng, epsilon);
 		real reward = 0.0;
 		int outcome = -1;
         for (int t=0; t<horizon; t++) {
