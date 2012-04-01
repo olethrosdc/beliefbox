ee282ffbc2ef7483f18cf97375f7e8c7cd842eb3
christos.dimitrakakis <christos.dimitrakakis@44875545-593f-0410-a76b-43d55c7074bc>
2012-04-01 18:07:34 +0000

    fixed a bug

--- src/statistics/Bounds.cc
+++ src/statistics/Bounds.cc
@@ -9,3 +9,11 @@ real WeissmanBound(int n_outcomes, int n_samples, real delta)
 	}
 	return 1.0;
 }
+
+real HoeffdingBound(int n_outcomes, int n_samples, real delta)
+{
+	if (n_samples > 0) {
+		return sqrt(log((real) n_outcomes / delta) / (2.0 * (real) n_samples));
+	}
+	return 1.0;
+}
