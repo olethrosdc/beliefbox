f8b2a6032a1b719bd85fb97a684d744ae9465001
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2012-06-18 15:35:11 +0000

    test chain

--- trunk/src/algorithms/tests/scripts/sample_test_chain.sh
+++ trunk/src/algorithms/tests/scripts/sample_test_chain.sh
@@ -5,44 +5,55 @@ epsilon=0.0
 
 task=Chain
 
-outdir=$HOME/results/$task/Normal
+outdir=$HOME/results/$task/Fixed
 mkdir -p $outdir
 
 
-# params="--environment $task --maze_name $HOME/projects/beliefbox/dat/$maze --gamma $gamma --epsilon 0.1 --n_runs $runs --n_steps $steps --n_episodes 10000 --episode_steps -1 --pit_value 0.0 --step_value 0.001"
+params="--environment $task --maze_name $HOME/projects/beliefbox/dat/$maze --gamma $gamma --epsilon 0.1 --n_runs $runs --n_steps $steps --n_episodes 10000 --episode_steps -1 --pit_value 0.0 --step_value 0.001"
 
-# for algorithm in Oracle QLearning Model UCRL
-# do
-#     echo $algorithm
-#     /usr/bin/time -o $outdir/${algorithm}.cpu ./bin/online_algorithms --algorithm $algorithm $params >$outdir/${algorithm}.out 
-# done
-
-# for algorithm in Oracle QLearning Model UCRL
-# do
-#     grep PAYOFF $outdir/${algorithm}.out > $outdir/${algorithm}.payoff
-#     grep RUN $outdir/${algorithm}.out > $outdir/${algorithm}.reward
-#     rm $outdir/${algorithm}.out
-# done
-
-params="--environment $task --maze_name $HOME/projects/beliefbox/dat/$maze --gamma $gamma --epsilon 0.0 --n_runs $runs --n_steps $steps --n_episodes 10000 --episode_steps -1 --pit_value 0.0 --step_value 0.001 --reward_prior Normal"
-for i in 001 002 004 008 016 032 064 128
-do 
-    for algorithm in LSampling USampling
-    do
-        echo $algorithm $i; 
-        /usr/bin/time -o $outdir/${algorithm}${i}.cpu  ./bin/online_algorithms --algorithm $algorithm --max_samples=${i} $params >$outdir/${algorithm}${i}.out &
-    done 
-    wait;
+for algorithm in Oracle QLearning Model UCRL
+do
+    echo $algorithm
+    /usr/bin/time -o $outdir/${algorithm}.cpu ./bin/online_algorithms --algorithm $algorithm $params >$outdir/${algorithm}.out 
 done
 
-wait;
+for algorithm in Oracle QLearning Model UCRL
+do
+    grep PAYOFF $outdir/${algorithm}.out > $outdir/${algorithm}.payoff
+    grep RUN $outdir/${algorithm}.out > $outdir/${algorithm}.reward
+    rm $outdir/${algorithm}.out
+done
 
-for i in 001 002 004 008 016 032 064 128
-do 
-    for algorithm in LSampling USampling
-    do
-        grep PAYOFF $outdir/${algorithm}${i}.out >$outdir/${algorithm}${i}.payoff; 
-        grep RUN $outdir/${algorithm}${i}.out >$outdir/${algorithm}${i}.reward; 
-        rm $outdir/${algorithm}${i}.out
+
+for prior in Fixed Normal Beta
+do
+    outdir=$HOME/results/$task/$prior
+    mkdir -p $outdir
+    
+    params="--environment $task --maze_name $HOME/projects/beliefbox/dat/$maze --gamma $gamma --epsilon 0.0 --n_runs $runs --n_steps $steps --n_episodes 10000 --episode_steps -1 --pit_value 0.0 --step_value 0.001 --reward_prior $prior"
+
+
+
+    for i in 001 002 004 008 016 032 064 128
+    do 
+        for algorithm in LSampling USampling
+        do
+            echo $algorithm $i; 
+            /usr/bin/time -o $outdir/${algorithm}${i}.cpu  ./bin/online_algorithms --algorithm $algorithm --max_samples=${i} $params >$outdir/${algorithm}${i}.out &
+        done 
+        wait;
     done
+
+    wait;
+
+    for i in 001 002 004 008 016 032 064 128
+    do 
+        for algorithm in LSampling USampling
+        do
+            grep PAYOFF $outdir/${algorithm}${i}.out >$outdir/${algorithm}${i}.payoff; 
+            grep RUN $outdir/${algorithm}${i}.out >$outdir/${algorithm}${i}.reward; 
+            rm $outdir/${algorithm}${i}.out
+        done
+    done
+
 done
