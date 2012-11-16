c81b1c3143efeca00f1ac3b2ff431778a93ab3e7
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2012-11-16 17:05:57 +0000

    minor changes

--- src/algorithms/tests/scripts/rsapi_test.sh
+++ src/algorithms/tests/scripts/rsapi_test.sh
@@ -6,16 +6,17 @@ n_iter=25
 horizon=1000
 for n_states in 10 20 50 100 200 500 1000
 do
-    for n_rollouts in 10 100 #1000
+    for n_rollouts in 10 20 50 100 200 #1000
     do
         for environment in MountainCar Pendulum
         do
+            echo "$environment s:$n_states n:$n_rollouts"
             outdir=$resdir/$environment/
             mkdir -p $outdir
             dst=$outdir/s${n_states}_r${n_rollouts}
-            cmd="$HOME/projects/beliefbox/src/algorithms/tests/bin/rsapi --environment $environment --n_states $n_states --n_rollouts $n_rollouts --gamma $gamma --horizon $horizon --n_iter $n_iter"
+            cmd="$HOME/projects/beliefbox/src/algorithms/tests/bin/rsapi --environment $environment --group --n_states $n_states --n_rollouts $n_rollouts --gamma $gamma --horizon $horizon --n_iter $n_iter"
             echo ${cmd} >$dst.cmd
-            for run in 1 #2 3 4 5 6 7 8 
+            for run in 1 2 #3 4 5 6 7 8 
             do
                 nice -n 19 ${cmd} >${dst}_run${run}.out &
             done
