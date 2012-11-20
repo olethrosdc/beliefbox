f92ea7babab08cd8826248a39c96d99fae4e1ef0
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2012-11-20 13:32:33 +0000

    RSAPI

--- src/algorithms/tests/scripts/rsapi_test.sh
+++ src/algorithms/tests/scripts/rsapi_test.sh
@@ -14,12 +14,16 @@ do
             outdir=$resdir/$environment/
             mkdir -p $outdir
             dst=$outdir/s${n_states}_r${n_rollouts}
-            cmd="$HOME/projects/beliefbox/src/algorithms/tests/bin/rsapi --environment $environment --group --n_states $n_states --n_rollouts $n_rollouts --gamma $gamma --horizon $horizon --n_iter $n_iter"
+            cmd="$HOME/projects/beliefbox/src/algorithms/tests/bin/rsapi --environment $environment --n_states $n_states --n_rollouts $n_rollouts --gamma $gamma --horizon $horizon --n_iter $n_iter"
             echo ${cmd} >$dst.cmd
-            for run in 1 2 #3 4 5 6 7 8 
-            do
-                nice -n 19 ${cmd} >${dst}_run${run}.out &
-            done
+            nice -n 19 ${cmd} >${dst}_run${run}.out &
+            nice -n 19 ${cmd} --group >${dst}_run${run}_group.out &
+            nice -n 19 ${cmd} --group --resample >${dst}_run${run}_group_resample.out &
+
+            nice -n 19 ${cmd} --lipschitz 1 >${dst}_run${run}_lipschitz.out &
+            nice -n 19 ${cmd} --group --lipschitz 1 >${dst}_run${run}_group_lipschitz.out &
+            nice -n 19 ${cmd} --group --resample --lipschitz 1 >${dst}_run${run}_group_resample_lipschitz.out &
+
             wait;
         done
     done
