66a7cc48b2c314434d223be3df90795fe46d5e88
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2012-11-30 21:30:44 +0000

    - added representative state model implementation - need to test implementation

--- src/models/RepresentativeStateModel.h
+++ src/models/RepresentativeStateModel.h
@@ -20,22 +20,66 @@ class RepresentativeStateModel : class MDP<S, A>
 protected:
     Model model;
     std::vector<S> states;
+	uint n_actions;
     Vector V;
+	DiscreteMDP* mdp;
+
 public:
-    RepresentativeStateModel(const Model& model_, const std::vector<S>& states_) :
+    RepresentativeStateModel(const Model& model_, const std::vector<S>& states_, uint n_actions_) :
         model(model_),
-        states(states_)
+        states(states_),
+		n_actions(n_actions_),
+		mdp(NULL)
     {
+		assert(n_actions > 0);
     }
+	~RepresentativeStateModel()
+	{ 
+		if (mdp) {
+			delete mdp;
+		}
+	}
+	
     void AddState(const S& state)
     {
         states.push_back(state);
     }
-    ComputeStateValues(real threshold, int max_iter = -1)
+    
+	void ComputeStateValues(real gamma, real threshold, int max_iter = -1)
     {
-        ValueIteration VI;
-        MDP
-    }
+		if (mdp) {
+			delete mdp;
+		}
+
+        mdp = new DiscreteMDP (states.size(), n_actions);
+	
+		for (uint i=0; i<states.size(); ++i) {
+			for (uint a=0; a<n_actions; ++a) {
+				Vector p(states.size());
+				for (uint j=0; j<states.size(); ++j) {
+					p(j) = model.getTransitionProbability(i, a, j);
+				}
+				p /= p.Sum();
+				for (uint j=0; j<states.size(); ++j) {
+					setTransitionProbability(i, a, j, p(j));
+				}
+			}
+		}
+
+		ValueIteration value_iteration(mdp, gamma);
+		value_iteration.ComputeStateValues(threshold, max_iter);
+		V = value_iteration.V;
+	}
+
+	real getValue(const S& state, const A& action)
+	{
+		Vector p(states.size());
+		for (uint j=0; j<states.size(); ++j) {
+			p(j) = model.getTransitionProbability(i, a, j);
+		}
+		p /= p.Sum();
+		return p * V;
+	}
     
 };
 
