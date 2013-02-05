4c1c09c539eb0f5fe5d0c8306b0943a19ce447ba
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-02-05 14:44:00 +0000

    added double loop

--- trunk/src/algorithms/tests/srp.cc
+++ trunk/src/algorithms/tests/srp.cc
@@ -48,6 +48,7 @@
 #include "RiverSwim.h"
 #include "OptimisticTask.h"
 #include "InventoryManagement.h"
+#include "DoubleLoop.h"
 
 // -- Continuous environments -- //
 #include "MountainCar.h"
@@ -95,7 +96,7 @@ Statistics EvaluateAlgorithm (int n_episodes,
 static const char* const help_text = "Usage: online_algorithms [options] algorithm environment\n\
 \nOptions:\n\
     --algorithm:    {QLearning, Model, Sarsa, LSampling, USampling, UCRL, TdBma}\n\
-    --environment:  {MountainCar, ContextBandit, RandomMDP, Gridworld, Chain, Optimistic, RiverSwim, Inventory}\n\
+    --environment:  {MountainCar, ContextBandit, RandomMDP, Gridworld, Chain, Optimistic, RiverSwim, Inventory, DoubleLoop}\n\
     --n_states:     number of states (usually there is no need to specify it)\n\
     --n_actions:    number of actions (usually there is no need to specify it)\n\
     --gamma:        reward discounting in [0,1]\n\
@@ -325,6 +326,8 @@ int main (int argc, char** argv)
             environment = new OptimisticTask (0.1, 0.01);
         } else if (!strcmp(environment_name, "RiverSwim")) { 
             environment = new RiverSwim();
+        } else if (!strcmp(environment_name, "DoubleLoop")) { 
+            environment = new DoubleLoop();
         } else if (!strcmp(environment_name, "Inventory")) { 
             int period = n_actions - 1;
             int max_items = n_states - 1;
