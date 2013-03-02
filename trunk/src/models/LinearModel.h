99adbf8645e08859268942ae47988a62b31362ec
ntziorzi@gmail.com <ntziorzi@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-03-02 17:14:40 +0000

    Linear model

--- trunk/src/models/LinearModel.h
+++ trunk/src/models/LinearModel.h
@@ -26,25 +26,32 @@ protected:
 	int d;					///< Output dimensions of the transition model
 	std::vector<Matrix> M;	///< Mean Matrix of the transition model
 	std::vector<Matrix> V;	///< Covariance Matrix of the transition model
+	std::vector<Matrix> SP;  
 	std::vector<Vector> MR; ///< Mean of the reward model
 	std::vector<Matrix> VR; ///< Covariance Matrix of the reward model
+	std::vector<Matrix> SR;
 	RBFBasisSet* RBFs;
 	Environment<S, A>* environment;
+	std::vector<BayesianMultivariateRegression*> regression_t;
 public:
-	LinearModel(const int& m_, const int& d_, RBFBasisSet* RBFs_, Environment<S,A>* environment_) : RBFs(RBFs_), environment(environment_)
+	LinearModel(const int& m_, const int& d_, RBFBasisSet* RBFs_, Environment<S,A>* environment_, std::vector<BayesianMultivariateRegression*> regression_t_) : RBFs(RBFs_), environment(environment_), regression_t(regression_t_)
 	{
 		m = m_;
 		d = d_;
 		int n_actions = environment->getNActions();
 		M.resize(n_actions);
 		V.resize(n_actions);
+		SP.resize(n_actions);
 		MR.resize(n_actions);
 		VR.resize(n_actions);
+		SR.resize(n_actions);
 		for(int i=0; i<n_actions; ++i) {
 			M[i]	= Matrix::Null(d,m);
 			V[i]	= Matrix::Unity(d,d);
+			SP[i]	= Matrix::Unity(m,m);
 			MR[i]	= Vector::Null(m);
 			VR[i]	= Matrix::Unity(1,1);
+			SR[i]	= Matrix::Unity(1,1);
 		}
 	}
 	LinearModel(const int& m_, const int& d_, Environment<S,A>* environment_) : environment(environment_)
@@ -54,25 +61,42 @@ public:
 		int n_actions = environment->getNActions();
 		M.resize(n_actions);
 		V.resize(n_actions);
+		SP.resize(n_actions);
 		MR.resize(n_actions);
 		VR.resize(n_actions);
+		SR.resize(n_actions);
 		for(int i=0; i<n_actions; ++i) {
 			M[i]	= Matrix::Null(d,m);
 			V[i]	= Matrix::Unity(d,d);
+			SP[i]	= Matrix::Unity(m,m);
 			MR[i]	= Vector::Null(m);
 			VR[i]	= Matrix::Unity(1,1);
+			SR[i]	= Matrix::Unity(1,1);
 		}
 		RBFs = NULL;
 	}
-	LinearModel(const std::vector<Matrix>& M_, const std::vector<Matrix>& V_, const std::vector<Vector>& MR_, const std::vector<Matrix>& VR_, RBFBasisSet* RBFs_, Environment<S,A>* environment_)
-		: M(M_), V(V_), MR(MR_), VR(VR_), RBFs(RBFs_), environment(environment_)
+	LinearModel(const std::vector<Matrix>& M_, 
+				const std::vector<Matrix>& V_, 
+				const std::vector<Matrix>& SP_,
+				const std::vector<Vector>& MR_, 
+				const std::vector<Matrix>& VR_,
+				const std::vector<Matrix>& SR_,
+				RBFBasisSet* RBFs_, 
+				Environment<S,A>* environment_)
+		: M(M_), V(V_), SP(SP_), MR(MR_), VR(VR_), SR(SR_), RBFs(RBFs_), environment(environment_)
 	{
 		d = M[0].Rows();
 		m = M[0].Columns();
 		//*TODO*/
 	}
-	LinearModel(const std::vector<Matrix>& M_, const std::vector<Matrix>& V_, const std::vector<Vector>& MR_, const std::vector<Matrix>& VR_,Environment<S,A>* environment_)
-	: M(M_), V(V_), MR(MR_), VR(VR_), environment(environment_)
+	LinearModel(const std::vector<Matrix>& M_, 
+				const std::vector<Matrix>& V_, 
+				const std::vector<Matrix>& SP_,
+				const std::vector<Vector>& MR_, 
+				const std::vector<Matrix>& VR_,
+				const std::vector<Matrix>& SR_,
+				Environment<S,A>* environment_)
+	: M(M_), V(V_), SP(SP_), MR(MR_), VR(VR_), SR(SR_), environment(environment_)
 	{
 		d = M[0].Rows();
 		m = M[0].Columns();
@@ -85,8 +109,10 @@ public:
 		for(int i=0; i<n_actions; ++i) {
 			M[i]	= Matrix::Null(d,m);
 			V[i]	= Matrix::Unity(d,d);
+			SP[i]	= Matrix::Unity(m,m);
 			MR[i]	= Vector::Null(m);
 			VR[i]	= Matrix::Unity(1,1);
+			SR[i]	= Matrix::Unity(1,1);
 		}
 	}
 	const void SetStatePredictionMean(const Matrix& M_, const int& a)
@@ -102,6 +128,10 @@ public:
 		}
 		M = M_;
 	}
+	Matrix GetStatePredictionVar(const int& a)
+	{
+		return V[a];
+	}
 	const void SetStatePredictionVar(const Matrix& V_, const int& a)
 	{
 		assert( V_.Rows() == d && V_.Columns() == d);
@@ -114,6 +144,19 @@ public:
 			assert(V_[i].Rows() == d && V_[i].Columns() == d);
 		}
 		V = V_;
+	}
+	const void SetStatePredictionS(const Matrix& SP_, const int& a)
+	{
+		assert(SP_.Rows() == m && SP_.Columns() == m);
+		SP[a] = SP_;
+	}
+	const void SetStatePredictionS(const std::vector<Matrix>& SP_)
+	{
+		int n_actions = environment->getNActions();
+		for( int i=0; i<n_actions; ++i) {
+			assert(SP_[i].Rows() == m && SP_[i].Columns() == m);
+		}
+		SP = SP_;
 	}
 	const void SetRewardPredictionMean(const Vector& MR_, const int& a)
 	{
@@ -130,41 +173,75 @@ public:
 	}
 	const void SetRewardPredictionVar(const Matrix& VR_, const int& a)
 	{
-		assert(VR_.Rows() == d && VR_.Columns() == d);
+		assert(VR_.Rows() == 1 && VR_.Columns() == 1);
 		VR[a] = VR_;
 	}
 	const void SetRewardPredictionVar(const std::vector<Matrix>& VR_)
 	{
 		int n_actions = environment->getNActions();
 		for( int i=0; i<n_actions; ++i) {
-			assert(VR_[i].Rows() == d && VR_[i].Columns() == d);
+			assert(VR_[i].Rows() == 1 && VR_[i].Columns() == 1);
 		}
 		VR = VR_;
 	}
-	const void SetModelParameters(const Matrix& M_, const Matrix& V_, const Vector& MR_, const Matrix& VR_, int a)
+	const void SetRewardPredictionS(const Matrix& SR_, const int& a)
+	{
+		assert(SR_.Rows() == 1 && SR_.Columns() == 1);
+		SR[a] = SR_;
+	}
+	const void SetRewardPredictionS(const std::vector<Matrix>& SR_)
+	{
+		int n_actions = environment->getNActions();
+		for( int i=0; i<n_actions; ++i) {
+			assert(SR_[i].Rows() == 1 && SR_[i].Columns() == 1);
+		}
+		SR = SR_;
+	}
+	
+	const void SetModelParameters(const Matrix& M_, 
+								  const Matrix& V_, 
+								  const Matrix& SP_,
+								  const Vector& MR_, 
+								  const Matrix& VR_,
+								  const Matrix& SR_,
+								  int a)
 	{
 		assert(M_.Rows() == d && M_.Columns() == m);
 		assert(V_.Rows() == d && V_.Columns() == d);
+		assert(SP_.Rows() == m && SP_.Columns() == m);
 		assert(MR_.Size() == m);
-		assert(VR_.Rows() == d && VR_.Columns() == d);
+		assert(VR_.Rows() == 1 && VR_.Columns() == 1);
+		assert(SR_.Rows() == 1 && SR_.Columns() == 1);
 		M[a]	= M_;
 		V[a]	= V_;
+		SP[a]	= SP_;
 		MR[a]	= MR_;
 		VR[a]	= VR_;
+		SR[a]	= SR_;
 	}
-	const void SetModelParameters(const std::vector<Matrix>& M_, const std::vector<Matrix>& V_, const std::vector<Vector>& MR_,  const std::vector<Matrix>& VR_)
+	
+	const void SetModelParameters(const std::vector<Matrix>& M_,
+								  const std::vector<Matrix>& V_, 
+								  const std::vector<Matrix>& SP_,
+								  const std::vector<Vector>& MR_,  
+								  const std::vector<Matrix>& VR_,
+								  const std::vector<Matrix>& SR_)
 	{
 		int n_actions = environment->getNActions();
 		for( int i=0; i<n_actions; ++i) {
 			assert(M_[i].Rows() == d && M_[i].Columns() == m);
 			assert(V_[i].Rows() == d && V_[i].Columns() == d);
+			assert(SP_[i].Rows() == m && SP_[i].Columns() == m);
 			assert(MR_[i].Size() == m);
-			assert(VR_[i].Rows() == d && VR_[i].Columns() == d);
+			assert(VR_[i].Rows() == 1 && VR_[i].Columns() == 1);
+			assert(SR_[i].Rows() == 1 && SR_[i].Columns() == 1);
 		}
 		M	= M_;
 		V	= V_;
+		SP	= SP_;
 		MR	= MR_;
 		VR	= VR_;
+		SR  = SR_;
 	}
 	const void SetEnvrironment(const Environment<Vector,int>& environment_)
 	{
@@ -178,10 +255,15 @@ public:
     {
         return environment->StateLowerBound();
     }	
+	const Vector getNextState(const Vector& state, const int& action)
+	{
+		return regression_t[action]->generate(state);
+	}
 	virtual real getTransitionProbability(const S& state, const A& action, const S& next_state) const
 	{
+		//Basis function creation
 		Vector phi;
-		if(RBFs != NULL)	{
+		if(RBFs != NULL) {
 			RBFs->Evaluate(state);
 			phi = RBFs->F();
 		}
@@ -190,32 +272,43 @@ public:
 		}
 		phi.Resize(m);
 		phi[m-1] = 1.0;
-		
+
 		Vector mean = M[action] * phi;
-		
-		//printf("mean");
-//		mean.print(stdout);
-//		printf("state");
-//		state.print(stdout);
-		
-		MultivariateNormal m_normal(mean, V[action].Inverse_LU());
+
+		real c = 1.0;
+		//Matrix xx = OuterProduct(phi,phi);
+//		xx = (xx + SP[action]).Inverse();
+//		Vector xx1 = xx*phi;
+//		c = 1.0 / (c - Product(phi,xx1));
+		MultivariateNormal m_normal(mean,(c*V[action]).Inverse());
+//		MultivariateNormal m_normal(mean,(c*VV).Inverse());
 		return m_normal.pdf(next_state);
 	}
 	virtual real getExpectedReward(const Vector& state, const int& action) const
 	{
-		Vector phi;
-		if(RBFs != NULL)	{
-			RBFs->Evaluate(state);
-			phi = RBFs->F();
-		}
-		else {
-			phi = state;
-		}
-		phi.Resize(m);
-		phi[m-1] = 1.0;
-		
-		return Product(MR[action],phi);
+		//Vector phi;
+//	
+//		if(RBFs != NULL) {
+//			RBFs->Evaluate(state);
+//			phi = RBFs->F();
+//		}
+//		else {
+//			phi = state;
+//		}
+//		phi.Resize(m);
+//		phi[m-1] = 1.0;
+//		
+//		return Product(MR[action],phi);
+		Vector previous_state = environment->getState();
+		environment->Reset();
+		environment->setState(state);
+		environment->Act(action);
+		real reward = environment->getReward();
+		environment->Reset();
+		environment->setState(previous_state);
+		return reward;
 	}
+
 };
 
 
--- trunk/src/models/RepresentativeStateModel.h
+++ trunk/src/models/RepresentativeStateModel.h
@@ -14,9 +14,11 @@
 #define REPRESENTATIVE_STATE_MODEL_H
 
 #include "MDP.h"
+#include "Grid.h"
 #include "DiscreteMDP.h"
 #include "ValueIteration.h"
 #include "Vector.h"
+#include "FittedValueIteration.h"
 
 /** RSM 
 
@@ -43,17 +45,23 @@ protected:
 	Model& model;				///< model to use to build discrete approximation from
 	uint init_samples;			///< initial number of collected samples 
     std::vector<S> states;		///< set of representative states
+	std::vector<Vector> v;		///< number of visits on each representative state
+	std::vector<Vector> f;		///< number of times each representative state transit to a terminal state;
 	uint n_actions;				///< number of actions (Note: what to do for continuous?)
     Vector V;					///< value vector cache
+	FittedValueIteration<S,A>* fitted_value_iteration;
 	DiscreteMDP* mdp;			///< pointer to approximate model
 public:
     /// Build a model from a set of representative states
-    RepresentativeStateModel(real gamma_, real threshold_, Model& model_, const std::vector<S>& states_, uint n_actions_) :
-        gamma(gamma_),
+    RepresentativeStateModel(real gamma_, real threshold_, Model model_, const std::vector<S>& states_, const std::vector<Vector>& v_, const std::vector<Vector>& f_, uint n_actions_, const FittedValueIteration<S,A>* fitted_value_iteration_):
+		gamma(gamma_),
 		threshold(threshold_),
         model(model_),
         states(states_),
+		v(v_),
+		f(f_),
 		n_actions(n_actions_),
+		fitted_value_iteration(fitted_value_iteration_),	
         mdp(NULL)
         //mdp(states.size(), n_actions)
     {
@@ -67,25 +75,30 @@ public:
 							 real threshold_,
 							 Model& model_,
                              uint n_states,
-                             uint n_actions_) :
+                             uint n_actions_,
+							 FittedValueIteration<S,A>* fitted_value_iteration_ = NULL) :
         gamma(gamma_),
 		threshold(threshold_),
         model(model_),
 		n_actions(n_actions_),
+		fitted_value_iteration(fitted_value_iteration_),
         mdp(NULL)
     {
         assert(n_actions > 0);
         S lower_bound = model.StateLowerBound();
         S upper_bound = model.StateUpperBound();
 		init_samples = n_states;
-        //logmsg("[%d %d]\n", lower_bound, upper_bound);
+			Vector stats(3);
+
         for (uint i=0; i<n_states; ++i) {
-            //S state = i % (int) upper_bound;
-            S state = urandom(lower_bound, upper_bound);
-            printf("%d -> %d\n", i, state);
+			
+            S state = urandom(lower_bound, upper_bound);	
+			v.push_back(stats);
+			f.push_back(stats);
             states.push_back(state);
         }
-        BuildMDP();
+		
+		BuildMDP();
 		ComputeStateValues();
     }
 
@@ -97,16 +110,21 @@ public:
 							 Model model_,
                              Sampler& sampler,
                              uint n_states,
-                             uint n_actions_) :
+                             uint n_actions_,
+							 FittedValueIteration<S,A>* fitted_value_iteration_) :
         gamma(gamma_),
 		threshold(threshold_),
         model(model_),
 		n_actions(n_actions_),
+		fitted_value_iteration(fitted_value_iteration_),
         mdp(NULL)
     {
         assert(n_actions > 0);
+		Vector stats(3,1);
 		init_samples = n_states;
         for (uint i=0; i<n_states; ++i) {
+			v.push_back(stats);
+			f.push_back(stats);
             states.push_back(sampler.Generate());
         }
         BuildMDP();
@@ -123,18 +141,32 @@ public:
 	void Reset()
 	{
 		states.resize(init_samples);
+		Vector stats(3);
+		v.resize(init_samples);
+		f.resize(init_samples);
 		S lower_bound = model.StateLowerBound();
         S upper_bound = model.StateUpperBound();
 		for (uint i=0; i<init_samples; ++i) {
-            S state = urandom(lower_bound, upper_bound);
+            //S state(2);
+			S state = urandom(lower_bound, upper_bound);
+			//Theta
+			//state[0] =  urandom(-1, 1);
+			// dTheta/dt
+			//state[1] = urandom(-0.001, 0.001);
+			v.push_back(stats);
+			f.push_back(stats);
             states.push_back(state);
         }
-		BuildMDP();
+//		BuildMDP();
+//		ComputeStateValues();
 	}
 	
     /// Add a new state to the representative set
     void AddState(const S& state)
     {
+		Vector stats(3);
+		v.push_back(stats);
+		f.push_back(stats);
         states.push_back(state);
 		//BuildMDP();
 //		ComputeStateValues();
@@ -144,36 +176,54 @@ public:
     void BuildMDP()
     {
         int n_states = states.size();
-        mdp = new DiscreteMDP(n_states, n_actions);
-        Vector p(n_states);
-//        printf("Build model\n");
+		//real alpha = 0.5;
+//		real beta  = 0.5;
+//        mdp = new DiscreteMDP(n_states + 1, n_actions);
+		mdp = new DiscreteMDP(n_states, n_actions);
+//      printf("Build model\n");
 		for (int i=0; i<n_states; ++i) {
 			for (uint a=0; a<n_actions; ++a) {
+				Vector p(n_states);
+//				Vector p(n_states + 1);
+//				real p0 = (f[i][a] + alpha) / (v[i][a] + alpha + beta);
+				//p0 = 0.0;
                 // Set rewards
                 //logmsg("Getting reward\n");
                 real r_ia = model.getExpectedReward(states[i], a);
-                //logmsg("Got it!\n");
-                mdp->setFixedReward(i, a, r_ia);
 
+                mdp->setFixedReward(i, a, r_ia);
+				
                 // Set transition probabilities
 				for (int j=0; j<n_states; ++j) {
 					p(j) =  model.getTransitionProbability(states[i], a, states[j]);
 				}
-                //logmsg ("s:%d a:%d r:(%f %f) ", i, a, r_ia,  model.getExpectedReward(states[i], a)); p.print(stdout);
-                real sum = p.Sum();
+//				logmsg ("s:%d a:%d r:(%f %f) ", i, a, r_ia,  model.getExpectedReward(states[i], a)); p.print(stdout);
+				
+				real sum = p.Sum();
+
 				if (sum > 0) {
                     p /= sum;
                 } else {
-                    //Swarning("OUCH\n");
+//                    Swarning("OUCH\n");
                     p += 1.0;
                     p /= p.Sum();
                 }
-				logmsg ("s:%d a:%d r:(%f %f) ", i, a, r_ia,  model.getExpectedReward(states[i], a)); p.print(stdout);
+
+//				p = p*(1-p0);
+//				p[n_states] = p0;
+//				logmsg ("s:%d a:%d r:(%f %f) ", i, a, r_ia,  model.getExpectedReward(states[i], a)); p.print(stdout);
+
                 mdp->setTransitionProbabilities(i, a, p, 1e-3);
 			}
 		}
-
+		//for(uint a=0; a<n_actions; ++a) {
+//			Vector p(n_states + 1);
+//			p(n_states) = 1.0;
+//			mdp->setFixedReward(n_states,a,0.0);
+//			mdp->setTransitionProbabilities(n_states,a,p,1e-3);
+//		}
         mdp->Check();
+		mdp->ShowModel();
     }
 	
 	void Update(Model& model_)
@@ -182,24 +232,35 @@ public:
 		BuildMDP();
 		ComputeStateValues();
 	}
-
-	void ComputeStateValues(int max_iter = -1)
+	void UpdateStatistics(const int& state, const int& action, const bool& endsim) 
+	{
+		v[state][action]++;
+		if(!endsim) 
+			f[state][action]++;
+	}
+	void ComputeStateValues(int max_iter = 300)
     {
 		ValueIteration value_iteration(mdp, gamma);
-		value_iteration.ComputeStateValuesStandard(threshold, max_iter);
-        for (int i=0; i<(int) states.size(); ++i) {
-            printf("%f\n", value_iteration.getValue(i));
-        }
+		value_iteration.Reset();
+		value_iteration.ComputeStateValues(threshold, max_iter);
 		V = value_iteration.V;
+		printf("Value state\n");
+        logmsg("AV: "); V.print(stdout);
+	//	int n_states = states.size();
+//		V = Vector(n_states);
+//		for(int i =0; i<n_states;i++)
+//			V[i] = fitted_value_iteration->getValue(states[i]);
+//		fitted_value_iteration->Update(threshold,max_iter);
+//		
 		//printf("Value state\n");
-        //logmsg("AV: "); V.print(stdout);
+		//logmsg("AV: "); V.print(stdout);
 	}
 
 	real getValue(const S& state, const A& action)
 	{
 		Vector p((int) states.size());
 		for (uint j=0; j<states.size(); ++j) {
-			p(j) = model.getTransitionProbability(state, action, states[j]);
+			p(j)  = model.getTransitionProbability(state, action, states[j]);
 		}
         real sum = p.Sum();
         
@@ -210,8 +271,11 @@ public:
             p /= p.Sum();
         }
         real r = model.getExpectedReward(state, action);
-
+		
         real U = Product(p, V);
+//		Vector next_state = model.getNextState(state,action);
+//		real U = fitted_value_iteration->getValue(next_state);
+		
 		return  r + gamma * U;
 	}
 
@@ -219,23 +283,21 @@ public:
 	{
         Vector Q(n_actions);
         for (uint i=0; i<n_actions; ++i) {
-            Q(i) = getValue(state, i);
+            Q[i] = getValue(state, i);
         }
+		Q.print(stdout);
         return Max(Q);
 	}
 	
-	std::vector<S> getRepresentativesSamples()
-	{
+	std::vector<S> getRepresentativesSamples() {
 		return states;
 	}
 	
-	int getNSamples()
-	{
+	int getNSamples() {
 		return states.size();
 	}
-	S getSample(int i)
-	{
-		assert(i >= 0 && i < states.size());
+	S getSample(int i) {
+		assert(i >= 0 && i < (int)states.size());
 		return states[i];
 	}
 };
