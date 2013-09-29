99eb2380e53cdbcf30151d61d234716cda84e063
ntziorzi <ntziorzi@44875545-593f-0410-a76b-43d55c7074bc>
2013-09-29 22:03:05 +0000

    lala

--- trunk/src/algorithms/tests/GPRL_Offline_Pendulum.sh
+++ trunk/src/algorithms/tests/GPRL_Offline_Pendulum.sh
@@ -20,7 +20,7 @@ do
     do
 	outdir=./GPRLPendulumOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --environment Pendulum --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
+	./bin/GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
 	mv offline_GPRL_RESULTS_STEPS_Pendulum $outdir
 	mv offline_GPRL_RESULTS_REWARDS_Pendulum, $outdir
     done
