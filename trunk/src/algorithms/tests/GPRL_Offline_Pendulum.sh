152749a7208c1abadf7a33f1a0ec6a16e780ea14
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-09-30 14:06:40 +0000

    oops

--- trunk/src/algorithms/tests/GPRL_Offline_Pendulum.sh
+++ trunk/src/algorithms/tests/GPRL_Offline_Pendulum.sh
@@ -10,7 +10,7 @@ do
 	outdir=./GPRLPendulumOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
 	cd $outdir
-	sem -j 8 --id gprl_mutex GPRL GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
+	sem -j 8 --id gprl_mutex GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
@@ -21,7 +21,7 @@ do
 	outdir=./GPRLPendulumOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
 	cd $outdir
-	sem -j 8 --id gprl_mutex GPRL GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
+	sem -j 8 --id gprl_mutex GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
