ceae946609210acbc08ac3220eb7060d5ba5bd75
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-09-30 14:19:05 +0000

    fixed scripts and everything else too

--- src/algorithms/tests/GPRL_Online_Mountain.sh
+++ src/algorithms/tests/GPRL_Online_Mountain.sh
@@ -1,23 +1,17 @@
 #! /bin/bash
 
-make DBG_OPT=DBG -j8  GPRL
-
 for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
 	outdir=./GPRLMountainCarOnline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file ????
-	mv online_GPRL_RESULTS_STEPS_MountainCar $outdir
-	mv online_GPRL_RESULTS_REWARDS_MountainCar $outdir
+	./bin/GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin
 done
 
 for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
     outdir=./GPRLMountainCarOnline/$n_rollout/$n_runs
     mkdir -p $outdir
-    ./bin/GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file ????
-    mv online_GPRL_RESULTS_STEPS_MountainCar $outdir
-    mv online_GPRL_RESULTS_REWARDS_MountainCar $outdir
+    sem -j 8 --id gprl_mutex GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin  2>&1 >out.log
 done
 
 
--- src/algorithms/tests/GPRL_Online_Pendulum.sh
+++ src/algorithms/tests/GPRL_Online_Pendulum.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-make DBG_OPT=DBG -j8  GPRL
-
 for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
     outdir=./GPRLPendulumOnline/$n_rollout/$n_runs
     mkdir -p $outdir
-    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
+    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin
     mv online_GPRL_RESULTS_STEPS_Pendulum $outdir
     mv online_GPRL_RESULTS_REWARDS_Pendulum $outdir
 done
@@ -15,7 +13,7 @@ for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
     outdir=./GPRLPendulumOnline/$n_rollout/$n_runs
     mkdir -p $outdir
-    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
+    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin
     mv online_GPRL_RESULTS_STEPS_Pendulum $outdir
     mv online_GPRL_RESULTS_REWARDS_Pendulum $outdir
 done
