4758a82dd2e0177d454b07a05b0e912d57482466
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-09-30 14:06:26 +0000

    added SEM

--- src/algorithms/tests/GPRL_Offline_Pendulum.sh
+++ src/algorithms/tests/GPRL_Offline_Pendulum.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-make DBG_OPT=DBG -j8  GPRL
+##make DBG_OPT=DBG -j8  GPRL
 
+resdir=$HOME/results/GPRL
 for((n_rollout=10;n_rollout<=45;n_rollout = n_rollout+5)) 
 do
     for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
     do
 	outdir=./GPRLPendulumOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
-	mv offline_GPRL_RESULTS_STEPS_Pendulum $outdir
-	mv offline_GPRL_RESULTS_REWARDS_Pendulum $outdir
+	cd $outdir
+	sem -j 8 --id gprl_mutex GPRL GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
@@ -20,9 +20,8 @@ do
     do
 	outdir=./GPRLPendulumOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file ????
-	mv offline_GPRL_RESULTS_STEPS_Pendulum $outdir
-	mv offline_GPRL_RESULTS_REWARDS_Pendulum, $outdir
+	cd $outdir
+	sem -j 8 --id gprl_mutex GPRL GPRL --environment Pendulum --discount 0.95 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
