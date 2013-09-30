9e0effad7fc138187b22de6da88ada4b12b7593b
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-09-30 14:04:48 +0000

    updated with SEM

--- src/algorithms/tests/GPRL_Offline_Mountain.sh
+++ src/algorithms/tests/GPRL_Offline_Mountain.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-make DBG_OPT=DBG -j8  GPRL
+##make DBG_OPT=DBG -j8  GPRL
+
+resdir=$HOME/results/GPRL
 
 for((n_rollout=10;n_rollout<=45;n_rollout = n_rollout+5)) 
 do
     for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
     do
-	outdir=./GPRLMountainCarOffline/$n_rollout/$n_runs
+	outdir=$resdir/GPRLMountainCarOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file ????
-	mv offline_GPRL_RESULTS_STEPS_MountainCar $outdir
-	mv offline_GPRL_RESULTS_REWARDS_MountainCar $outdir
+	cd $outdir
+	sem -j 8 --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
@@ -18,11 +19,10 @@ for((n_rollout=50;n_rollout<=1000;n_rollout = n_rollout+50))
 do
     for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
     do
-	outdir=./GPRLMountainCarOffline/$n_rollout/$n_runs
+	outdir=$resdir/GPRLMountainCarOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file ????
-	mv offline_GPRL_RESULTS_STEPS_MountainCar $outdir
-	mv offline_GPRL_RESULTS_REWARDS_MountainCar $outdir
+	cd $outdir
+	sem -j 8 --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
