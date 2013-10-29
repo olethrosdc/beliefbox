c3d0f3e7e1be651b766d6e62459c212c129cfa38
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-10-29 10:53:02 +0000

    scripts for lia ssThis line, and those below, will be ignored--
    M    src/algorithms/tests/GPRL_Offline_Pendulum.sh
    M    src/algorithms/tests/GPRL_Offline_Mountain.sh
    M    src/algorithms/tests/GPRL_Online_Pendulum.sh
    M    src/algorithms/tests/GPRL_Online_Mountain.sh

--- src/algorithms/tests/GPRL_Online_Pendulum.sh
+++ src/algorithms/tests/GPRL_Online_Pendulum.sh
@@ -1,21 +1,13 @@
 #! /bin/bash
 
-for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
-do
-    outdir=./GPRLPendulumOnline/$n_rollout/$n_runs
-    mkdir -p $outdir
-    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin
-    mv online_GPRL_RESULTS_STEPS_Pendulum $outdir
-    mv online_GPRL_RESULTS_REWARDS_Pendulum $outdir
-done
+resdir=$HOME/results/GPRL
 
 for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
-    outdir=./GPRLPendulumOnline/$n_rollout/$n_runs
+    outdir=$resdir/GPRLPendulumOnline/$n_rollout/$n_runs
     mkdir -p $outdir
-    ./bin/GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed_file $HOME/data/r1e7.bin
-    mv online_GPRL_RESULTS_STEPS_Pendulum $outdir
-    mv online_GPRL_RESULTS_REWARDS_Pendulum $outdir
+    cd $outdir
+    sem -j 8 --id gprl_mutex GPRL --online --environment Pendulum --discount 0.95 --n_training_episodes 1000 --n_training_e_steps 3000 --grid 3 --scale 0.5 --seed $n_runs --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
 done
 
 
