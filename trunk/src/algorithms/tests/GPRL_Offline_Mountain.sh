b6181cb135b7292348f7e2e4e9cf7c5a6d69709a
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-10-29 10:53:02 +0000

    scripts for lia ssThis line, and those below, will be ignored--
    M    src/algorithms/tests/GPRL_Offline_Pendulum.sh
    M    src/algorithms/tests/GPRL_Offline_Mountain.sh
    M    src/algorithms/tests/GPRL_Online_Pendulum.sh
    M    src/algorithms/tests/GPRL_Online_Mountain.sh

--- trunk/src/algorithms/tests/GPRL_Offline_Mountain.sh
+++ trunk/src/algorithms/tests/GPRL_Offline_Mountain.sh
@@ -3,26 +3,25 @@
 ##make DBG_OPT=DBG -j8  GPRL
 
 resdir=$HOME/results/GPRL
-
-for((n_rollout=10;n_rollout<=45;n_rollout = n_rollout+5)) 
+n_cpu=48
+for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
-    for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
+    for((n_rollout=10;n_rollout<=45;n_rollout = n_rollout+5)) 
     do
+	
 	outdir=$resdir/GPRLMountainCarOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
 	cd $outdir
-	sem -j 8 --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
+	sem -j $n_cpu --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed $n_runs --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
-done
-
-for((n_rollout=50;n_rollout<=1000;n_rollout = n_rollout+50)) 
-do
-    for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
+    
+    
+    for((n_rollout=50;n_rollout<=1000;n_rollout = n_rollout+50)) 
     do
 	outdir=$resdir/GPRLMountainCarOffline/$n_rollout/$n_runs
 	mkdir -p $outdir
 	cd $outdir
-	sem -j 8 --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
+	sem -j $n_cpu --id gprl_mutex GPRL --environment MountainCar --discount 0.999 --n_training_episodes $n_rollout --n_training_e_steps 40 --n_testing_episodes 1000 --n_testing_e_steps 1000 --grid 4 --scale 1.0 --seed $n_runs --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
     done
 done
 
--- trunk/src/algorithms/tests/GPRL_Online_Mountain.sh
+++ trunk/src/algorithms/tests/GPRL_Online_Mountain.sh
@@ -1,17 +1,10 @@
 #! /bin/bash
 
+resdir=$HOME/results/GPRL
 for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
 do
-	outdir=./GPRLMountainCarOnline/$n_rollout/$n_runs
+	outdir=$resdir/GPRLMountainCarOnline/$n_rollout/$n_runs
 	mkdir -p $outdir
-	./bin/GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin
+	cd $outdir
+	sem -j 8 --id gprl_mutex GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed $n_runs --seed_file $HOME/data/r1e7.bin 2>&1 >out.log
 done
-
-for((n_runs = 1; n_runs<=100; n_runs = n_runs + 1))
-do
-    outdir=./GPRLMountainCarOnline/$n_rollout/$n_runs
-    mkdir -p $outdir
-    sem -j 8 --id gprl_mutex GPRL --online --environment MountainCar --discount 0.999 --n_training_episodes 1000 --n_training_e_steps 1000 --grid 4 --scale 1.0 --seed_file $HOME/data/r1e7.bin  2>&1 >out.log
-done
-
-
