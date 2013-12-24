f0ceb6a87645aea46b99aa833042d546814d57a6
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2013-12-24 14:02:56 +0000

    Missing file

---
+++ src/models/StateAction.h
@@ -0,0 +1,45 @@
+// -*- Mode: c++  -*-
+#ifndef STATE_ACTION_H
+#define STATE_ACTION_H
+
+#include "HashCombine.h"
+
+/// A convenience class
+template<typename S, typename A>
+class StateAction
+{
+public:
+	int state;
+	int action;
+	StateAction(int s, int a)
+		: state(s), action(a)
+	{
+	}
+	bool operator==(const StateAction& rhs) const
+	{
+		if ((rhs.state != state) || (rhs.action != action)) {
+			return false;
+		}
+		return true;
+	}
+};
+
+typedef StateAction<int, int> DiscreteStateAction;
+
+template <>
+struct std::hash<DiscreteStateAction>
+{
+	/// The hash is a shift-xor of two hashes
+	std::size_t operator() (const DiscreteStateAction& s) const
+	{
+		std::size_t seed = 0;
+		hash_combine(seed, std::hash<int>()(s.state));
+		hash_combine(seed, std::hash<int>()(s.action));
+		return seed;
+	}
+	
+};
+
+
+
+#endif
