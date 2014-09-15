37d276d982cfef462a3c01d6dd13de56db1b282c
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2014-09-15 13:09:59 +0000

    a script to test UCTMC

--- trunk/src/algorithms/tests/scripts/compare_uctmc.sh
+++ trunk/src/algorithms/tests/scripts/compare_uctmc.sh
@@ -6,7 +6,7 @@ depth=100
 grid=10
 horizon=1000
 
-for environment in MountainCar Pendumlum Puddle Bicycle CartPole Acrobot
+for environment in MountainCar Pendulum PuddleWorld Bike CartPole Acrobot
 do
 	./bin/UCTMC_test --environment $environment --discount $discount --depth $depth --grids $grid --horizon $horizon --n_rollouts $rollouts --n_evaluations 10 --n_episodes 100 >${environment}_UCTMC.out 
 done
