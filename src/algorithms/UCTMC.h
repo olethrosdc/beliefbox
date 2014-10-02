a17cbb5c61b05ce7a5885e6ffd1b7f40a30bc742
christos.dimitrakakis@gmail.com <christos.dimitrakakis@gmail.com@44875545-593f-0410-a76b-43d55c7074bc>
2014-10-02 20:50:14 +0000

    fixed an initialisation problem. now it works fine.

--- src/algorithms/UCTMC.h
+++ src/algorithms/UCTMC.h
@@ -27,101 +27,101 @@ template <class S, class A>
 class UCTMC
 {
 private:
-  real gamma;                    // Discount factor.
-  real c_uct;
-  ContinuousStateEnvironment* environment; // The environment model.
-  RandomNumberGenerator* rng;    // Random Number generator.
-  EvenGrid discretize;
-  real learning_rate; // Learning rate.
+	real gamma;                    // Discount factor.
+	real c_uct;
+	ContinuousStateEnvironment* environment; // The environment model.
+	RandomNumberGenerator* rng;    // Random Number generator.
+	EvenGrid discretize;
+	real learning_rate; // Learning rate.
 	real lambda; // mixing
-  int nBins;  // Number of grids per state space dimension.
-  int MaxDepth;                  // Maximum tree depth.
-  int NRollouts; // Number of sampled rollouts.
-  int nActions;  // Number of available actions.
-  Matrix Q;    // State-Actions values.
-  Matrix C;    // Counters.
+	int nBins;  // Number of grids per state space dimension.
+	int MaxDepth;                  // Maximum tree depth.
+	int NRollouts; // Number of sampled rollouts.
+	int nActions;  // Number of available actions.
+	Matrix Q;    // State-Actions values.
+	Matrix C;    // Counters.
 public:
 	UCTMC(const real& gamma_, const real& c_uct_, ContinuousStateEnvironment* environment_, RandomNumberGenerator* rng_, const EvenGrid &discretize_, const real& learning_rate_, const real& lambda_, const int& MaxDepth_, const int& NRollouts_)
-    : gamma(gamma_),
-      c_uct(c_uct_),
-      environment(environment_),
-      rng(rng_),
-      discretize(discretize_),
-      learning_rate(learning_rate_),
-      lambda(lambda_),
-      MaxDepth(MaxDepth_),
-      NRollouts(NRollouts_)
-  {
-    nActions = environment->getNActions();    
+		: gamma(gamma_),
+		  c_uct(c_uct_),
+		  environment(environment_),
+		  rng(rng_),
+		  discretize(discretize_),
+		  learning_rate(learning_rate_),
+		  lambda(lambda_),
+		  MaxDepth(MaxDepth_),
+		  NRollouts(NRollouts_)
+	{
+		nActions = environment->getNActions();    
 
-    Q = Matrix(discretize.getNIntervals(), nActions);
-    C = Matrix(discretize.getNIntervals(), nActions);
-  };
-  ~UCTMC() {}
-  // Reset statistics
-  void UCT_Reset() {
-    Q = Matrix(discretize.getNIntervals(), nActions);
-    C = Matrix(discretize.getNIntervals(), nActions);
-  };
+		Q = Matrix(discretize.getNIntervals(), nActions);
+		C = Matrix(discretize.getNIntervals(), nActions);
+	};
+	~UCTMC() {}
+	// Reset statistics
+	void UCT_Reset() {
+		Q = Matrix(discretize.getNIntervals(), nActions);
+		C = Matrix(discretize.getNIntervals(), nActions);
+	};
 
-  real UCT_Search(S state, int depth, bool terminal){
+	real UCT_Search(S state, int depth, bool terminal){
 
-    if(depth >= MaxDepth ||  terminal) {
-      return 0.0;
-    }
-    int index = discretize.getInterval(state);
-    real SampleReturn = 0;
+		if(depth >= MaxDepth ||  terminal) {
+			return 0.0;
+		}
+		int index = discretize.getInterval(state);
+		real SampleReturn = 0;
 
-    int bestAction = 0;
-    real bestValue = Q(index, bestAction) + 2*c_uct*sqrt( log(C.RowSum(index)) / (C(index,bestAction)+1) );
+		int bestAction = 0;
+		real bestValue = Q(index, bestAction) + 2*c_uct*sqrt( log(C.RowSum(index)) / (C(index,bestAction)+1) );
     
-    for(int a = 1; a < nActions; ++a) {
-      real value = Q(index, a) + 2*c_uct*sqrt( log(C.RowSum(index)) / (C(index,a)+1) );
-      if(value > bestValue) {
-	bestValue = value;
-	bestAction = a;
-      }
-    }
-    //environment->Reset();
+		for(int a = 1; a < nActions; ++a) {
+			real value = Q(index, a) + 2*c_uct*sqrt( log(C.RowSum(index)) / (C(index,a)+1) );
+			if(value > bestValue) {
+				bestValue = value;
+				bestAction = a;
+			}
+		}
+		//environment->Reset();
     
-    bool running = environment->Act(bestAction);
-    S nextState  = environment->getState();
+		bool running = environment->Act(bestAction);
+		S nextState  = environment->getState();
 
-    real reward  = environment->getReward();
+		real reward  = environment->getReward();
    
-    SampleReturn = reward + gamma*UCT_Search(nextState, depth + 1, !running);
-    C(index, bestAction) += 1;
-    Q(index, bestAction) += (SampleReturn - Q(index, bestAction))/ C(index, bestAction);
-	// Note: it's best to use learning-rate when lambda < 1.
+		SampleReturn = reward + gamma*UCT_Search(nextState, depth + 1, !running);
+		C(index, bestAction) += 1;
+		Q(index, bestAction) += (SampleReturn - Q(index, bestAction))/ C(index, bestAction);
+		// Note: it's best to use learning-rate when lambda < 1.
 	
-	//Q(index, bestAction) = (1-learning_rate)*Q(index, bestAction) + learning_rate*SampleReturn;
-	real LambdaReturn = lambda * SampleReturn + (1 - lambda) * Q(index, bestAction);
-    return LambdaReturn;
-  };
-  int PlanPolicy(const S& state) {
-    int rollouts = 0;
+		//Q(index, bestAction) = (1-learning_rate)*Q(index, bestAction) + learning_rate*SampleReturn;
+		real LambdaReturn = lambda * SampleReturn + (1 - lambda) * Q(index, bestAction);
+		return LambdaReturn;
+	};
+	int PlanPolicy(const S& state) {
+		int rollouts = 0;
 
-    do {
+		do {
   
-      UCT_Search(state, 0, false);
-      environment->Reset();
-      environment->setState(state);
-      rollouts++;
-    } while(rollouts < NRollouts);
+			UCT_Search(state, 0, false);
+			environment->Reset();
+			environment->setState(state);
+			rollouts++;
+		} while(rollouts < NRollouts);
 
-    int bestAction = 0;
-    int index = discretize.getInterval(state);
-    real bestValue = Q(index,0);
+		int bestAction = 0;
+		int index = discretize.getInterval(state);
+		real bestValue = Q(index,0);
 
-    for(int a = 1; a < nActions; ++a) {
-      real value = Q(index,a);
-      if(value > bestValue) {
-	bestValue = value;
-	bestAction = a;
-      }
-    }
-    return bestAction;
-  };
+		for(int a = 1; a < nActions; ++a) {
+			real value = Q(index,a);
+			if(value > bestValue) {
+				bestValue = value;
+				bestAction = a;
+			}
+		}
+		return bestAction;
+	};
 };
 
 #endif
