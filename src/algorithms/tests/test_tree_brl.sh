5a3d733b5a709f24cb96a941302def77bcc0fb0e
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2017-12-21 17:33:13 +0100

    all tests

--- src/algorithms/tests/test_tree_brl.sh
+++ src/algorithms/tests/test_tree_brl.sh
@@ -5,7 +5,7 @@ summary=test_tree_brl.dat
 rm -f $summary
 for horizon in 0 1 2 4 6 8 10 12 14 16
 do
-	for leaf in 0 #2 3 4 5
+	for leaf in 0 2 3 4 5
 	do
 		fname=out-${horizon}-${leaf}
 		./bin/test_tree_brl $horizon $leaf >$fname
