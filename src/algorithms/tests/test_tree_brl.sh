f58d6912a8cc0b804e257e635dc446edb0cd6e3c
Christos Dimitrakakis <chrdimi@chalmers.se>
2018-04-03 11:17:04 +0200

    minor script changes

--- src/algorithms/tests/test_tree_brl.sh
+++ src/algorithms/tests/test_tree_brl.sh
@@ -2,13 +2,12 @@
 
 ulimit -Sv 4000000
 n=100
-for horizon in 0 1 2 3 3 4 5 6 8 10 12 14 16
+for horizon in 0 1 2 3 3 4 5 6 8 
 do
 	for leaf in 0 2 3 4 5
 	do
 		fname=out-${horizon}-${leaf}
-		sem -j8 ./bin/test_tree_brl ${horizon} ${leaf} $n >$fname 
+		sem -j6 ./bin/test_tree_brl ${horizon} ${leaf} $n >$fname 
 	done
-	wait
 done
 
