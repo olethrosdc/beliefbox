f6eb1be260c2c76e1169b0fa4d47f53c2a19ed8b
Christos Dimitrakakis <chrdimi@chalmers.se>
2020-03-09 16:03:01 +0100

    misc

--- src/algorithms/tests/scripts/frozen_lake.sh
+++ src/algorithms/tests/scripts/frozen_lake.sh
@@ -1,20 +1,21 @@
 runs=100
-outdir=frozen_lake_0
+outdir=frozen_lake_minus
 n_steps=100000
 episode_steps=1000
-n_episodes=100
+n_episodes=1000
 
-
+mkdir -p $outdir
 
 for alg in QLearning Model 
 do
-	time ./bin/online_algorithms --n_episodes $n_episodes --episode_steps $episode_steps --n_steps $n_steps --n_runs $runs --algorithm $alg --environment Gridworld --maze_name ~/projects/beliefbox/dat/frozen_lake  --step_value 0 --pit_value 0 --goal_value 1 --randomness 0.33 >$outdir/$alg.out
+	time ./bin/online_algorithms --n_episodes $n_episodes --episode_steps $episode_steps --n_steps $n_steps --n_runs $runs --algorithm $alg --environment Gridworld --maze_name ~/projects/beliefbox/dat/frozen_lake  --step_value -0.1 --pit_value -1 --goal_value 1 --randomness 0.33 >$outdir/$alg.out
 done
 
-for samples in 1 2 4 8
-do
-	for alg in USampling LSampling
-	do
-		time ./bin/online_algorithms --n_episodes $n_episodes --episode_steps $episode_steps --n_steps $n_steps --n_runs $runs --algorithm $alg --max_samples $samples--environment Gridworld --maze_name ~/projects/beliefbox/dat/frozen_lake --step_value 0 --pit_value -0 --goal_value 1 --randomness 0.33 >$outdir/${alg}_${samples}s.out
-	done
-done
+
+# for samples in 1 2 4 8
+# do
+# 	for alg in USampling LSampling
+# 	do
+# 		time ./bin/online_algorithms --n_episodes $n_episodes --episode_steps $episode_steps --n_steps $n_steps --n_runs $runs --algorithm $alg --max_samples $samples--environment Gridworld --maze_name ~/projects/beliefbox/dat/frozen_lake --step_value 0 --pit_value -0 --goal_value 1 --randomness 0.33 >$outdir/${alg}_${samples}s.out
+# 	done
+# done
