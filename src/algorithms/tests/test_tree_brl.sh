f4d403f06486274f367e82944b0a30ef2e93b13f
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2020-06-06 22:23:07 +0200

    Script tested for bandit and works

--- src/algorithms/tests/test_tree_brl.sh
+++ src/algorithms/tests/test_tree_brl.sh
@@ -2,14 +2,16 @@
 
 ulimit -Sv 4000000
 n=1
-T=10000
+T=100000
 gamma=0.95
 for horizon in 0 1 2 3 3 4 5 6 8 
 do
 	for leaf in 0 2 3 4 5
 	do
-		fname=out-${horizon}-${leaf}
-		sem -j6 ./bin/test_tree_brl ${horizon} ${leaf} $n $T $gamma >$fname 
+		fname=tbrl-${horizon}-${leaf}
+		#sem -j4
+		./bin/test_tree_brl ${horizon} ${leaf} $n $T $gamma >$fname.out
+		grep "# reward" $fname.out >$fname.reward
 	done
 done
 
