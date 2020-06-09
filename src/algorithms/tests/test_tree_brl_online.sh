dee949776652120913d060db13d9b96954e5dcc2
Christos Dimitrakakis <chrdimi@chalmers.se>
2020-06-09 23:48:20 +0200

    Still crashes with Normal prior, must check

--- src/algorithms/tests/test_tree_brl_online.sh
+++ src/algorithms/tests/test_tree_brl_online.sh
@@ -2,16 +2,21 @@
 
 ulimit -Sv 2000000
 env=Chain
-T=1000
+T=100
+runs=1
 for horizon in 1 2 3 4 5
 do
-	for state_samples in 1 2 3 
+	for state_samples in 1 2 3 4
 	do
-		for leaf in Min Mean UBound LBound
+		for reward_samples in 1 2
 		do
-			echo $horizon $state_samples
-			fname=tbrl-${env}-${horizon}h-${state_samples}s-${leaf}.out
-			sem -j 8 ./bin/online_algorithms --environment $env --n_episodes 1 --n_runs 10 --gamma 0.95 --n_steps $T --algorithm TBRL --horizon $horizon --max_samples $state_samples --leaf_value $leaf &>$fname
+			for leaf in Min MeanMDP UBound
+			do
+				echo $horizon $state_samples $reward_samples $leaf 
+				fname=tbrl-${env}-${horizon}h-${state_samples}s-${leaf}.out
+				#sem -j 8
+				./bin/online_algorithms --environment $env --n_episodes 1 --n_runs $runs --gamma 0.95 --n_steps $T --algorithm TBRL --horizon $horizon --max_samples $state_samples --max_reward_samples $reward_samples --leaf_value $leaf --reward_prior Beta &>$fname
+			done
 		done
 	done
 done
