9aa363555e461edbc5019622483ef6a0f43e722d
Christos Dimitrakakis <chrdimi@chalmers.se>
2020-06-09 23:50:54 +0200

    semaphore

--- src/algorithms/tests/test_tree_brl_online.sh
+++ src/algorithms/tests/test_tree_brl_online.sh
@@ -14,8 +14,7 @@ do
 			do
 				echo $horizon $state_samples $reward_samples $leaf 
 				fname=tbrl-${env}-${horizon}h-${state_samples}s-${reward_samples}r-${leaf}.out
-				#sem -j 8
-				./bin/online_algorithms --environment $env --n_episodes 1 --n_runs $runs --gamma 0.95 --n_steps $T --algorithm TBRL --horizon $horizon --max_samples $state_samples --max_reward_samples $reward_samples --leaf_value $leaf --reward_prior Beta &>$fname
+				sem -j 8 ./bin/online_algorithms --environment $env --n_episodes 1 --n_runs $runs --gamma 0.95 --n_steps $T --algorithm TBRL --horizon $horizon --max_samples $state_samples --max_reward_samples $reward_samples --leaf_value $leaf --reward_prior Beta &>$fname
 			done
 		done
 	done
