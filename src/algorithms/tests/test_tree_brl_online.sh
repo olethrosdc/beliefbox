efa32c688df038c3a607e5e996edd434a108b84b
Christos Dimitrakakis <chrdimi@chalmers.se>
2020-06-09 23:50:25 +0200

    Testing for 1000 steps on chain

--- src/algorithms/tests/test_tree_brl_online.sh
+++ src/algorithms/tests/test_tree_brl_online.sh
@@ -2,8 +2,8 @@
 
 ulimit -Sv 2000000
 env=Chain
-T=100
-runs=1
+T=1000
+runs=10
 for horizon in 1 2 3 4 5
 do
 	for state_samples in 1 2 3 4
@@ -13,7 +13,7 @@ do
 			for leaf in Min MeanMDP UBound
 			do
 				echo $horizon $state_samples $reward_samples $leaf 
-				fname=tbrl-${env}-${horizon}h-${state_samples}s-${leaf}.out
+				fname=tbrl-${env}-${horizon}h-${state_samples}s-${reward_samples}r-${leaf}.out
 				#sem -j 8
 				./bin/online_algorithms --environment $env --n_episodes 1 --n_runs $runs --gamma 0.95 --n_steps $T --algorithm TBRL --horizon $horizon --max_samples $state_samples --max_reward_samples $reward_samples --leaf_value $leaf --reward_prior Beta &>$fname
 			done
