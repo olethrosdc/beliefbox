c7a139ea214227ed330cd36b019448183f7afb17
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-04-04 22:19:32 +0200

    Bug in PGAC::Act

--- src/algorithms/OnlinePolicyGradient.cc
+++ src/algorithms/OnlinePolicyGradient.cc
@@ -29,6 +29,7 @@ real PolicyGradientActorCritic::Observe(real reward, int next_state, int next_ac
 
 int PolicyGradientActorCritic::Act(real reward, int next_state)
 {
+	policy.Observe(reward, next_state);
 	int next_action = policy.SelectAction();
 	Observe(reward, next_state, next_action);
 	return next_action;
@@ -56,6 +57,77 @@ real PolicyGradientActorCritic::GradientUpdate(int s, int a)
 }
 
 void PolicyGradientActorCritic::UpdatePolicy()
+{
+	for (int s=0; s<n_states; ++s) {
+		Vector eW = exp(params.getRow(s));
+		eW /= eW.Sum();
+		Vector* pS = policy.getActionProbabilitiesPtr(s);
+		(*pS) = eW;
+	}
+}
+
+PolicyGradientActorCriticPhi::PolicyGradientActorCriticPhi(BasisSet& basis_,
+														   int n_states_,
+														   int n_actions_,
+														   real gamma_,
+														   real step_size_) :
+	basis(basis_),
+	n_states(n_states_),
+	n_actions(n_actions_),
+	gamma(gamma_),
+	step_size(step_size_),
+	critic(n_states, n_actions, gamma, 0.0, step_size),
+	policy(n_states, n_actions),
+	params(n_states, n_actions),
+	Q(n_states, n_actions)
+{
+	Reset();
+}
+
+real PolicyGradientActorCriticPhi::Observe(real reward, int next_state, int next_action)
+{
+	basis.Observe(action, reward, next_state);
+	if (state >= 0) {
+		real d = GradientUpdate(state, action); // not sure how much difference it makes to update the next state-action pair instead
+		//printf("%d %d %d %d%f\n", state, action, d);
+		Q(state, action) += step_size * (reward + gamma * Q(next_state, next_action) - Q(state, action));
+	}
+	critic.Observe(reward, next_state, next_action);
+	UpdatePolicy();
+	state = next_state;
+	action = next_action;
+	return 0;
+}
+
+int PolicyGradientActorCriticPhi::Act(real reward, int next_state)
+{
+	int next_action = policy.SelectAction();
+	Observe(reward, next_state, next_action);
+	return next_action;
+}
+
+real PolicyGradientActorCriticPhi::GradientUpdate(Vector& s, int a)
+{
+	//real U = critic.getValue(s);
+	//real U = critic.getValue(s);
+	real U = Q(s,a);
+	printf("s:%d, a:%d: Q_w:%f Q_S:%f\n", s, a, U, critic.getValue(s,a));
+	real delta = 0;
+	for (int j=0; j<n_actions; ++j) {
+		real p_a = policy.getActionProbability(s, j);
+		real d_sj =  0;
+		if (j==a) {
+			d_sj = (1.0 - p_a) * U;
+		} else {
+			d_sj = -p_a * U;
+		}
+		params(s,j) += step_size * d_sj;
+		delta += fabs(d_sj);
+	}
+	return delta;
+}
+
+void PolicyGradientActorCriticPhi::UpdatePolicy()
 {
 	for (int s=0; s<n_states; ++s) {
 		Vector eW = exp(params.getRow(s));
